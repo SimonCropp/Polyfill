e753da313fda681857013551467cc209ddf98098
Simon Cropp <simon.cropp@gmail.com>
2023-03-15 22:04:33 +1100

    .

--- src/Tests/GlobalUsings.cs
+++ src/Tests/GlobalUsings.cs
@@ -2,3 +2,4 @@
 
 global using NUnit.Framework;
 global using Polyfill;
+global using System;
--- src/Tests/PolyExtensionsSample.cs
+++ src/Tests/PolyExtensionsSample.cs
@@ -2,6 +2,22 @@
 class PolyExtensionsSample
 {
     [Test]
+    public void SpanContains() =>
+        Assert.True("value".AsSpan().Contains('e'));
+
+    [Test]
+    public void SpanSequenceEqual() =>
+        Assert.True("value".AsSpan().SequenceEqual("value"));
+
+    [Test]
+    public void SpanStringBuilderAppend()
+    {
+        var builder = new StringBuilder();
+        builder.Append("value".AsSpan());
+        Assert.AreEqual("value", builder.ToString());
+    }
+
+    [Test]
     public void EndsWith() =>
         Assert.True("value".EndsWith('e'));
 
