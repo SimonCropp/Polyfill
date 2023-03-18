120ebe91eb50dfc78fd769e5ae8bbe5d7b9aacc6
Simon Cropp <simon.cropp@gmail.com>
2023-03-18 23:26:50 +1100

    .

--- src/Polyfill/PolyExtensions.cs
+++ src/Polyfill/PolyExtensions.cs
@@ -1,7 +1,5 @@
 // ReSharper disable RedundantUsingDirective
 // ReSharper disable PartialTypeWithSinglePart
-using System;
-using System.Text;
 using System.Diagnostics;
 using System.Diagnostics.CodeAnalysis;
 
@@ -11,31 +9,4 @@ namespace Polyfill;
 [DebuggerNonUserCode]
 static partial class PolyExtensions
 {
-#if NETFRAMEWORK || NETSTANDARD2_0
-
-    public static bool Contains(this string target, string value, StringComparison comparisonType) =>
-        target.IndexOf(value, comparisonType) >= 0;
-
-    public static bool StartsWith(this string target, char value)
-    {
-        if (target.Length == 0)
-        {
-            return false;
-        }
-
-        return target[0] == value;
-    }
-
-    public static bool EndsWith(this string target, char value)
-    {
-        if (target.Length == 0)
-        {
-            return false;
-        }
-
-        var lastPos = target.Length - 1;
-        return lastPos < target.Length &&
-               target[lastPos] == value;
-    }
-#endif
 }
