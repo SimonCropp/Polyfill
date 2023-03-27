4453994acaa62042665479b48d312def39fb3192
Simon Cropp <simon.cropp@gmail.com>
2023-03-27 20:27:26 +1100

    Update PolyfillExtensions_Stream.cs

--- src/Polyfill/PolyfillExtensions_Stream.cs
+++ src/Polyfill/PolyfillExtensions_Stream.cs
@@ -39,6 +39,13 @@ static partial class PolyfillExtensions
         return new(target.WriteAsync(segment.Array!, segment.Offset, segment.Count, cancellationToken));
     }
 
+    /// <summary>
+    /// Asynchronously reads the bytes from the current stream and writes them to another stream, using a specified
+    /// cancellation token. Both streams positions are advanced by the number of bytes copied.
+    /// </summary>
+    /// <param name="destination">The stream to which the contents of the current stream will be copied.</param>
+    /// <param name="cancellationToken">The token to monitor for cancellation requests. The default value is None.</param>
+    /// <returns></returns>
     public static Task CopyToAsync(
         this Stream target,
         Stream destination,
