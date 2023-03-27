8910411007c0b81326b2d3b2ad04a0aa867f7783
Simon Cropp <simon.cropp@gmail.com>
2023-03-27 20:32:14 +1100

    Update PolyfillExtensions_Stream.cs

--- src/Polyfill/PolyfillExtensions_Stream.cs
+++ src/Polyfill/PolyfillExtensions_Stream.cs
@@ -13,6 +13,20 @@ using System.Threading.Tasks;
 
 static partial class PolyfillExtensions
 {
+    /// <summary>
+    /// Asynchronously reads a sequence of bytes from the current stream, advances the position within the stream by
+    /// the number of bytes read, and monitors cancellation requests.
+    /// </summary>
+    /// <param name="buffer">The region of memory to write the data into.</param>
+    /// <param name="cancellationToken">
+    /// The token to monitor for cancellation requests. The default value is None.
+    /// </param>
+    /// <returns>
+    /// A task that represents the asynchronous read operation. The value of its Result property contains the
+    /// total number of bytes read into the buffer. The result value can be less than the number of bytes allocated in
+    /// the buffer if that many bytes are not currently available, or it can be 0 (zero) if the end of the stream has
+    /// been reached.
+    /// </returns>
     public static ValueTask<int> ReadAsync(
         this Stream target,
         Memory<byte> buffer,
@@ -26,6 +40,15 @@ static partial class PolyfillExtensions
         return new(target.ReadAsync(segment.Array!, segment.Offset, segment.Count, cancellationToken));
     }
 
+    /// <summary>
+    /// Asynchronously writes a sequence of bytes to the current stream, advances the current position
+    /// within this stream by the number of bytes written, and monitors cancellation requests.
+    /// </summary>
+    /// <param name="buffer">The region of memory to write data from.</param>
+    /// <param name="cancellationToken">
+    /// The token to monitor for cancellation requests. The default value is None.
+    /// </param>
+    /// <returns>A task that represents the asynchronous write operation.</returns>
     public static ValueTask WriteAsync(
         this Stream target,
         ReadOnlyMemory<byte> buffer,
