9badde6874d4992bb06491203ef7efc2be6e7a94
Simon Cropp <simon.cropp@gmail.com>
2023-03-29 18:46:55 +1100

    Resolve http global problem (#13)

---
+++ src/Polyfill/ResolveHttpGlobalProblem.cs
@@ -0,0 +1,19 @@
+#if NETFRAMEWORK
+
+#pragma warning disable
+
+// ReSharper disable RedundantUsingDirective
+// ReSharper disable PartialTypeWithSinglePart
+// ReSharper disable UnusedMember.Global
+
+using System.ComponentModel;
+
+namespace System.Net.Http
+{
+    [EditorBrowsable(EditorBrowsableState.Never)]
+    static class ResolveHttpGlobalProblem
+    {
+    }
+}
+
+#endif
