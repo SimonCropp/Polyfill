f7c35803d5682a207f987d3f9957320c638774b5
Tyrrrz <1935960+Tyrrrz@users.noreply.github.com>
2023-03-31 11:12:33 +0300

    optimize

--- src/Tests/PolyfillExtensionsTests_HttpClient.cs
+++ src/Tests/PolyfillExtensionsTests_HttpClient.cs
@@ -1,15 +1,29 @@
 ﻿// These tests cover HttpContent polyfills as well
 
+using System.Net;
 using System.Net.Http;
 
 partial class PolyfillExtensionsTests
 {
+    private class FakeHttpMessageHandler : HttpMessageHandler
+    {
+        protected override Task<HttpResponseMessage> SendAsync(
+            HttpRequestMessage request,
+            CancellationToken cancellationToken) => Task.FromResult(
+            new HttpResponseMessage(HttpStatusCode.OK)
+            {
+                RequestMessage = request,
+                Content = new StringContent("Fake Content")
+            }
+        );
+    }
+
     [Test]
     public async Task HttpClientGetStreamAsync_Positive()
     {
         // Arrange
         var cancellationToken = new CancellationToken();
-        using var httpClient = new HttpClient();
+        using var httpClient = new HttpClient(new FakeHttpMessageHandler(), true);
 
         // Act
         using var stream = await httpClient.GetStreamAsync("https://example.com", cancellationToken);
@@ -17,7 +31,7 @@ partial class PolyfillExtensionsTests
         var content = await reader.ReadToEndAsync();
 
         // Assert
-        StringAssert.Contains("Example Domain", content);
+        Assert.AreEqual("Fake Content", content);
     }
 
     [Test]
@@ -25,7 +39,7 @@ partial class PolyfillExtensionsTests
     {
         // Arrange
         var cancellationToken = new CancellationToken(true);
-        using var httpClient = new HttpClient();
+        using var httpClient = new HttpClient(new FakeHttpMessageHandler(), true);
 
         // Act
         try
@@ -45,14 +59,14 @@ partial class PolyfillExtensionsTests
     {
         // Arrange
         var cancellationToken = new CancellationToken();
-        using var httpClient = new HttpClient();
+        using var httpClient = new HttpClient(new FakeHttpMessageHandler(), true);
 
         // Act
         var bytes = await httpClient.GetByteArrayAsync("https://example.com", cancellationToken);
         var content = Encoding.UTF8.GetString(bytes);
 
         // Assert
-        StringAssert.Contains("Example Domain", content);
+        Assert.AreEqual("Fake Content", content);
     }
 
     [Test]
@@ -60,7 +74,7 @@ partial class PolyfillExtensionsTests
     {
         // Arrange
         var cancellationToken = new CancellationToken(true);
-        using var httpClient = new HttpClient();
+        using var httpClient = new HttpClient(new FakeHttpMessageHandler(), true);
 
         // Act
         try
@@ -80,13 +94,13 @@ partial class PolyfillExtensionsTests
     {
         // Arrange
         var cancellationToken = new CancellationToken();
-        using var httpClient = new HttpClient();
+        using var httpClient = new HttpClient(new FakeHttpMessageHandler(), true);
 
         // Act
         var content = await httpClient.GetStringAsync("https://example.com", cancellationToken);
 
         // Assert
-        StringAssert.Contains("Example Domain", content);
+        Assert.AreEqual("Fake Content", content);
     }
 
     [Test]
@@ -94,7 +108,7 @@ partial class PolyfillExtensionsTests
     {
         // Arrange
         var cancellationToken = new CancellationToken(true);
-        using var httpClient = new HttpClient();
+        using var httpClient = new HttpClient(new FakeHttpMessageHandler(), true);
 
         // Act
         try
