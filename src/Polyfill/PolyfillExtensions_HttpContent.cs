df7fa5bcc3e29687bf2b5d8d71004a041c60c457
Oleksii Holub <1935960+Tyrrrz@users.noreply.github.com>
2023-04-01 09:28:36 +0300

    Add polyfills for `HttpContent` & `HttpClient` (#16)

---
+++ src/Polyfill/PolyfillExtensions_HttpContent.cs
@@ -0,0 +1,63 @@
+﻿#if ((NETFRAMEWORK && HTTPREFERENCED) || NETSTANDARD2_0 || NETCOREAPP2X || NETCOREAPP3X)
+
+#pragma warning disable
+
+// ReSharper disable RedundantUsingDirective
+// ReSharper disable UnusedMember.Global
+
+using System.IO;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+
+static partial class PolyfillExtensions
+{
+    /// <summary>
+    /// Serializes the HTTP content and returns a stream that represents the content.
+    /// </summary>
+    /// <remarks>
+    /// Note that this method will internally buffer the content unless <c>CreateContentReadStreamAsync()</c> has been implemented to do otherwise.
+    /// </remarks>
+    /// <param name="cancellationToken">The token to monitor for cancellation requests. The default value is None.</param>
+    /// <returns>The task object representing the asynchronous operation.</returns>
+    public static async Task<Stream> ReadAsStreamAsync(
+        this HttpContent httpContent,
+        CancellationToken cancellationToken = default)
+    {
+        cancellationToken.ThrowIfCancellationRequested();
+        return await httpContent.ReadAsStreamAsync().ConfigureAwait(false);
+    }
+
+    /// <summary>
+    /// Serializes the HTTP content to a byte array as an asynchronous operation.
+    /// </summary>
+    /// <remarks>
+    /// Note that this method will internally buffer the content unless <c>CreateContentReadStreamAsync()</c> has been implemented to do otherwise.
+    /// </remarks>
+    /// <param name="cancellationToken">The token to monitor for cancellation requests. The default value is None.</param>
+    /// <returns>The task object representing the asynchronous operation.</returns>
+    public static async Task<byte[]> ReadAsByteArrayAsync(
+        this HttpContent httpContent,
+        CancellationToken cancellationToken = default)
+    {
+        cancellationToken.ThrowIfCancellationRequested();
+        return await httpContent.ReadAsByteArrayAsync().ConfigureAwait(false);
+    }
+
+    /// <summary>
+    /// Serializes the HTTP content to a string as an asynchronous operation.
+    /// </summary>
+    /// <remarks>
+    /// Note that this method will internally buffer the content unless <c>CreateContentReadStreamAsync()</c> has been implemented to do otherwise.
+    /// </remarks>
+    /// <param name="cancellationToken">The token to monitor for cancellation requests. The default value is None.</param>
+    /// <returns>The task object representing the asynchronous operation.</returns>
+    public static async Task<string> ReadAsStringAsync(
+        this HttpContent httpContent,
+        CancellationToken cancellationToken = default)
+    {
+        cancellationToken.ThrowIfCancellationRequested();
+        return await httpContent.ReadAsStringAsync().ConfigureAwait(false);
+    }
+}
+#endif
---
+++ src/Tests/PolyfillExtensionsTests_HttpClient.cs
@@ -0,0 +1,130 @@
+﻿// These tests cover HttpContent polyfills as well
+
+using System.Net;
+using System.Net.Http;
+
+partial class PolyfillExtensionsTests
+{
+    private class FakeHttpMessageHandler : HttpMessageHandler
+    {
+        protected override Task<HttpResponseMessage> SendAsync(
+            HttpRequestMessage request,
+            CancellationToken cancellationToken)
+        {
+            cancellationToken.ThrowIfCancellationRequested();
+
+            return Task.FromResult(
+                new HttpResponseMessage(HttpStatusCode.OK)
+                {
+                    RequestMessage = request,
+                    Content = new StringContent("Fake Content")
+                }
+            );
+        }
+    }
+
+    [Test]
+    public async Task HttpClientGetStreamAsync_Positive()
+    {
+        // Arrange
+        var cancellationToken = new CancellationToken();
+        using var httpClient = new HttpClient(new FakeHttpMessageHandler(), true);
+
+        // Act
+        using var stream = await httpClient.GetStreamAsync("https://example.com", cancellationToken);
+        using var reader = new StreamReader(stream);
+        var content = await reader.ReadToEndAsync();
+
+        // Assert
+        Assert.AreEqual("Fake Content", content);
+    }
+
+    [Test]
+    public async Task HttpClientGetStreamAsync_Negative()
+    {
+        // Arrange
+        var cancellationToken = new CancellationToken(true);
+        using var httpClient = new HttpClient(new FakeHttpMessageHandler(), true);
+
+        // Act
+        try
+        {
+            await httpClient.GetStreamAsync("https://example.com", cancellationToken);
+            Assert.Fail();
+        }
+        catch (OperationCanceledException ex)
+        {
+            // Assert
+            Assert.IsTrue(ex.CancellationToken.IsCancellationRequested);
+        }
+    }
+
+    [Test]
+    public async Task HttpClientGetByteArrayAsync_Positive()
+    {
+        // Arrange
+        var cancellationToken = new CancellationToken();
+        using var httpClient = new HttpClient(new FakeHttpMessageHandler(), true);
+
+        // Act
+        var bytes = await httpClient.GetByteArrayAsync("https://example.com", cancellationToken);
+        var content = Encoding.UTF8.GetString(bytes);
+
+        // Assert
+        Assert.AreEqual("Fake Content", content);
+    }
+
+    [Test]
+    public async Task HttpClientGetByteArrayAsync_Negative()
+    {
+        // Arrange
+        var cancellationToken = new CancellationToken(true);
+        using var httpClient = new HttpClient(new FakeHttpMessageHandler(), true);
+
+        // Act
+        try
+        {
+            await httpClient.GetByteArrayAsync("https://example.com", cancellationToken);
+            Assert.Fail();
+        }
+        catch (OperationCanceledException ex)
+        {
+            // Assert
+            Assert.IsTrue(ex.CancellationToken.IsCancellationRequested);
+        }
+    }
+
+    [Test]
+    public async Task HttpClientGetStringAsync_Positive()
+    {
+        // Arrange
+        var cancellationToken = new CancellationToken();
+        using var httpClient = new HttpClient(new FakeHttpMessageHandler(), true);
+
+        // Act
+        var content = await httpClient.GetStringAsync("https://example.com", cancellationToken);
+
+        // Assert
+        Assert.AreEqual("Fake Content", content);
+    }
+
+    [Test]
+    public async Task HttpClientGetStringAsync_Negative()
+    {
+        // Arrange
+        var cancellationToken = new CancellationToken(true);
+        using var httpClient = new HttpClient(new FakeHttpMessageHandler(), true);
+
+        // Act
+        try
+        {
+            await httpClient.GetStringAsync("https://example.com", cancellationToken);
+            Assert.Fail();
+        }
+        catch (OperationCanceledException ex)
+        {
+            // Assert
+            Assert.IsTrue(ex.CancellationToken.IsCancellationRequested);
+        }
+    }
+}
