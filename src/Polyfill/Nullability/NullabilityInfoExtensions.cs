218047fa1129f11a4c1bbd07bec3b4e6cb2c2d1c
Simon Cropp <simon.cropp@gmail.com>
2023-04-14 21:38:25 +1000

    Update NullabilityInfoExtensions.cs

--- src/Polyfill/Nullability/NullabilityInfoExtensions.cs
+++ src/Polyfill/Nullability/NullabilityInfoExtensions.cs
@@ -3,188 +3,190 @@
 
 using System.Collections.Concurrent;
 
-namespace System.Reflection
+namespace System.Reflection;
+
+/// <summary>
+/// Static and thread safe wrapper around <see cref="NullabilityInfoContext"/>.
+/// </summary>
+#if PolyPublic
+public
+#endif
+static class NullabilityInfoExtensions
 {
-    /// <summary>
-    /// Static and thread safe wrapper around <see cref="NullabilityInfoContext"/>.
-    /// </summary>
-    internal static class NullabilityInfoExtensions
+    static ConcurrentDictionary<ParameterInfo, NullabilityInfo> parameterCache = new();
+    static ConcurrentDictionary<PropertyInfo, NullabilityInfo> propertyCache = new();
+    static ConcurrentDictionary<EventInfo, NullabilityInfo> eventCache = new();
+    static ConcurrentDictionary<FieldInfo, NullabilityInfo> fieldCache = new();
+
+    public static NullabilityInfo GetNullabilityInfo(this MemberInfo info)
     {
-        static ConcurrentDictionary<ParameterInfo, NullabilityInfo> parameterCache = new();
-        static ConcurrentDictionary<PropertyInfo, NullabilityInfo> propertyCache = new();
-        static ConcurrentDictionary<EventInfo, NullabilityInfo> eventCache = new();
-        static ConcurrentDictionary<FieldInfo, NullabilityInfo> fieldCache = new();
-
-        internal static NullabilityInfo GetNullabilityInfo(this MemberInfo info)
+        if (info is PropertyInfo propertyInfo)
         {
-            if (info is PropertyInfo propertyInfo)
+            return propertyInfo.GetNullabilityInfo();
+        }
+
+        if (info is EventInfo eventInfo)
+        {
+            return eventInfo.GetNullabilityInfo();
+        }
+
+        if (info is FieldInfo fieldInfo)
+        {
+            return fieldInfo.GetNullabilityInfo();
+        }
+
+        throw new ArgumentException($"Unsupported type:{info.GetType().FullName}");
+    }
+
+    public static NullabilityState GetNullability(this MemberInfo info)
+    {
+        return GetReadOrWriteState(info.GetNullabilityInfo());
+    }
+
+    public static bool IsNullable(this MemberInfo info)
+    {
+        var nullability = info.GetNullabilityInfo();
+        return IsNullable(info.Name, nullability);
+    }
+
+    public static NullabilityInfo GetNullabilityInfo(this FieldInfo info)
+    {
+        return fieldCache.GetOrAdd(info, inner =>
+        {
+            var nullabilityContext = new NullabilityInfoContext();
+            return nullabilityContext.Create(inner);
+        });
+    }
+
+    public static NullabilityState GetNullability(this FieldInfo info)
+    {
+        return GetReadOrWriteState(info.GetNullabilityInfo());
+    }
+
+    public static bool IsNullable(this FieldInfo info)
+    {
+        var nullability = info.GetNullabilityInfo();
+        return IsNullable(info.Name, nullability);
+    }
+
+    public static NullabilityInfo GetNullabilityInfo(this EventInfo info)
+    {
+        return eventCache.GetOrAdd(info, inner =>
+        {
+            var nullabilityContext = new NullabilityInfoContext();
+            return nullabilityContext.Create(inner);
+        });
+    }
+
+    public static NullabilityState GetNullability(this EventInfo info)
+    {
+        return GetReadOrWriteState(info.GetNullabilityInfo());
+    }
+
+    public static bool IsNullable(this EventInfo info)
+    {
+        var nullability = info.GetNullabilityInfo();
+        return IsNullable(info.Name, nullability);
+    }
+
+    public static NullabilityInfo GetNullabilityInfo(this PropertyInfo info)
+    {
+        return propertyCache.GetOrAdd(info, inner =>
+        {
+            var nullabilityContext = new NullabilityInfoContext();
+            return nullabilityContext.Create(inner);
+        });
+    }
+
+    public static NullabilityState GetNullability(this PropertyInfo info)
+    {
+        return GetReadOrWriteState(info.GetNullabilityInfo());
+    }
+
+    public static bool IsNullable(this PropertyInfo info)
+    {
+        var nullability = info.GetNullabilityInfo();
+        return IsNullable(info.Name, nullability);
+    }
+
+    public static NullabilityInfo GetNullabilityInfo(this ParameterInfo info)
+    {
+        return parameterCache.GetOrAdd(info, inner =>
+        {
+            var nullabilityContext = new NullabilityInfoContext();
+            return nullabilityContext.Create(inner);
+        });
+    }
+
+    public static NullabilityState GetNullability(this ParameterInfo info)
+    {
+        return GetReadOrWriteState(info.GetNullabilityInfo());
+    }
+
+    public static bool IsNullable(this ParameterInfo info)
+    {
+        var nullability = info.GetNullabilityInfo();
+        return IsNullable(info.Name!, nullability);
+    }
+
+    static NullabilityState GetReadOrWriteState(NullabilityInfo nullability)
+    {
+        if (nullability.ReadState == NullabilityState.Unknown)
+        {
+            return nullability.WriteState;
+        }
+
+        return nullability.ReadState;
+    }
+
+    static NullabilityState GetKnownState(string name, NullabilityInfo nullability)
+    {
+        var readState = nullability.ReadState;
+        if (readState != NullabilityState.Unknown)
+        {
+            return readState;
+        }
+
+        var writeState = nullability.WriteState;
+        if (writeState != NullabilityState.Unknown)
+        {
+            return writeState;
+        }
+
+        throw new($"The nullability of '{nullability.Type.FullName}.{name}' is unknown. Assembly: {nullability.Type.Assembly.FullName}.");
+    }
+
+    static bool IsNullable(string name, NullabilityInfo nullability)
+    {
+        return GetKnownState(name, nullability) == NullabilityState.Nullable;
+    }
+
+    //Patching
+    internal static MemberInfo GetMemberWithSameMetadataDefinitionAs(Type type, MemberInfo member)
+    {
+        const BindingFlags all = BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Static | BindingFlags.Instance;
+        foreach (var info in type.GetMembers(all))
+        {
+            if (info.HasSameMetadataDefinitionAs(member))
             {
-                return propertyInfo.GetNullabilityInfo();
+                return info;
             }
-
-            if (info is EventInfo eventInfo)
-            {
-                return eventInfo.GetNullabilityInfo();
-            }
-
-            if (info is FieldInfo fieldInfo)
-            {
-                return fieldInfo.GetNullabilityInfo();
-            }
-
-            throw new ArgumentException($"Unsupported type:{info.GetType().FullName}");
         }
 
-        internal static NullabilityState GetNullability(this MemberInfo info)
-        {
-            return GetReadOrWriteState(info.GetNullabilityInfo());
-        }
+        throw new MissingMemberException(type.FullName, member.Name);
+    }
 
-        internal static bool IsNullable(this MemberInfo info)
-        {
-            var nullability = info.GetNullabilityInfo();
-            return IsNullable(info.Name, nullability);
-        }
+    //https://github.com/dotnet/runtime/blob/main/src/coreclr/System.Private.CoreLib/src/System/Reflection/MemberInfo.Internal.cs
+    static bool HasSameMetadataDefinitionAs(this MemberInfo target, MemberInfo other)
+    {
+        return target.MetadataToken == other.MetadataToken &&
+               target.Module.Equals(other.Module);
+    }
 
-        internal static NullabilityInfo GetNullabilityInfo(this FieldInfo info)
-        {
-            return fieldCache.GetOrAdd(info, inner =>
-            {
-                var nullabilityContext = new NullabilityInfoContext();
-                return nullabilityContext.Create(inner);
-            });
-        }
-
-        internal static NullabilityState GetNullability(this FieldInfo info)
-        {
-            return GetReadOrWriteState(info.GetNullabilityInfo());
-        }
-
-        internal static bool IsNullable(this FieldInfo info)
-        {
-            var nullability = info.GetNullabilityInfo();
-            return IsNullable(info.Name, nullability);
-        }
-
-        internal static NullabilityInfo GetNullabilityInfo(this EventInfo info)
-        {
-            return eventCache.GetOrAdd(info, inner =>
-            {
-                var nullabilityContext = new NullabilityInfoContext();
-                return nullabilityContext.Create(inner);
-            });
-        }
-
-        internal static NullabilityState GetNullability(this EventInfo info)
-        {
-            return GetReadOrWriteState(info.GetNullabilityInfo());
-        }
-
-        internal static bool IsNullable(this EventInfo info)
-        {
-            var nullability = info.GetNullabilityInfo();
-            return IsNullable(info.Name, nullability);
-        }
-
-        internal static NullabilityInfo GetNullabilityInfo(this PropertyInfo info)
-        {
-            return propertyCache.GetOrAdd(info, inner =>
-            {
-                var nullabilityContext = new NullabilityInfoContext();
-                return nullabilityContext.Create(inner);
-            });
-        }
-
-        internal static NullabilityState GetNullability(this PropertyInfo info)
-        {
-            return GetReadOrWriteState(info.GetNullabilityInfo());
-        }
-
-        internal static bool IsNullable(this PropertyInfo info)
-        {
-            var nullability = info.GetNullabilityInfo();
-            return IsNullable(info.Name, nullability);
-        }
-
-        internal static NullabilityInfo GetNullabilityInfo(this ParameterInfo info)
-        {
-            return parameterCache.GetOrAdd(info, inner =>
-            {
-                var nullabilityContext = new NullabilityInfoContext();
-                return nullabilityContext.Create(inner);
-            });
-        }
-
-        internal static NullabilityState GetNullability(this ParameterInfo info)
-        {
-            return GetReadOrWriteState(info.GetNullabilityInfo());
-        }
-
-        internal static bool IsNullable(this ParameterInfo info)
-        {
-            var nullability = info.GetNullabilityInfo();
-            return IsNullable(info.Name!, nullability);
-        }
-
-        static NullabilityState GetReadOrWriteState(NullabilityInfo nullability)
-        {
-            if (nullability.ReadState == NullabilityState.Unknown)
-            {
-                return nullability.WriteState;
-            }
-
-            return nullability.ReadState;
-        }
-
-        static NullabilityState GetKnownState(string name, NullabilityInfo nullability)
-        {
-            var readState = nullability.ReadState;
-            if (readState != NullabilityState.Unknown)
-            {
-                return readState;
-            }
-
-            var writeState = nullability.WriteState;
-            if (writeState != NullabilityState.Unknown)
-            {
-                return writeState;
-            }
-
-            throw new($"The nullability of '{nullability.Type.FullName}.{name}' is unknown. Assembly: {nullability.Type.Assembly.FullName}.");
-        }
-
-        static bool IsNullable(string name, NullabilityInfo nullability)
-        {
-            return GetKnownState(name, nullability) == NullabilityState.Nullable;
-        }
-
-        //Patching
-        internal static MemberInfo GetMemberWithSameMetadataDefinitionAs(Type type, MemberInfo member)
-        {
-            const BindingFlags all = BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Static | BindingFlags.Instance;
-            foreach (var info in type.GetMembers(all))
-            {
-                if (info.HasSameMetadataDefinitionAs(member))
-                {
-                    return info;
-                }
-            }
-
-            throw new MissingMemberException(type.FullName, member.Name);
-        }
-
-        //https://github.com/dotnet/runtime/blob/main/src/coreclr/System.Private.CoreLib/src/System/Reflection/MemberInfo.Internal.cs
-        static bool HasSameMetadataDefinitionAs(this MemberInfo target, MemberInfo other)
-        {
-            return target.MetadataToken == other.MetadataToken &&
-                   target.Module.Equals(other.Module);
-        }
-
-        //https://github.com/dotnet/runtime/issues/23493
-        internal static bool IsGenericMethodParameter(this Type target)
-        {
-            return target.IsGenericParameter &&
-                   target.DeclaringMethod != null;
-        }
+    //https://github.com/dotnet/runtime/issues/23493
+    internal static bool IsGenericMethodParameter(this Type target)
+    {
+        return target.IsGenericParameter &&
+               target.DeclaringMethod != null;
     }
 }
