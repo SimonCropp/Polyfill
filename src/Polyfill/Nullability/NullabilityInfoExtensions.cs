beee2d7999a51c4388d65fe3b5b5408719750d16
Simon Cropp <simon.cropp@gmail.com>
2023-04-14 21:39:00 +1000

    Update NullabilityInfoExtensions.cs

--- src/Polyfill/Nullability/NullabilityInfoExtensions.cs
+++ src/Polyfill/Nullability/NullabilityInfoExtensions.cs
@@ -160,33 +160,4 @@ static class NullabilityInfoExtensions
     {
         return GetKnownState(name, nullability) == NullabilityState.Nullable;
     }
-
-    //Patching
-    internal static MemberInfo GetMemberWithSameMetadataDefinitionAs(Type type, MemberInfo member)
-    {
-        const BindingFlags all = BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Static | BindingFlags.Instance;
-        foreach (var info in type.GetMembers(all))
-        {
-            if (info.HasSameMetadataDefinitionAs(member))
-            {
-                return info;
-            }
-        }
-
-        throw new MissingMemberException(type.FullName, member.Name);
-    }
-
-    //https://github.com/dotnet/runtime/blob/main/src/coreclr/System.Private.CoreLib/src/System/Reflection/MemberInfo.Internal.cs
-    static bool HasSameMetadataDefinitionAs(this MemberInfo target, MemberInfo other)
-    {
-        return target.MetadataToken == other.MetadataToken &&
-               target.Module.Equals(other.Module);
-    }
-
-    //https://github.com/dotnet/runtime/issues/23493
-    internal static bool IsGenericMethodParameter(this Type target)
-    {
-        return target.IsGenericParameter &&
-               target.DeclaringMethod != null;
-    }
 }
