6242174c8626b70987e4467ebe471a9e617aa8eb
Simon Cropp <simon.cropp@gmail.com>
2023-07-28 11:10:53 +1000

    add ReadLineAsync (#67)
    * add ReadLineAsync
    
    * Update Directory.Build.props

--- src/Polyfill/PolyfillExtensions_TextReader.cs
+++ src/Polyfill/PolyfillExtensions_TextReader.cs
@@ -64,5 +64,24 @@ static partial class PolyfillExtensions
 
         return target.ReadToEndAsync();
     }
+
+    /// <summary>
+    /// Reads a line of characters asynchronously and returns the data as a string.
+    /// </summary>
+    /// <param name="cancellationToken">The token to monitor for cancellation requests.</param>
+    /// <returns>A value task that represents the asynchronous read operation. The value of the <c>TResult</c>
+    /// parameter contains the next line from the text reader, or is <see langword="null" /> if all of the characters have been read.</returns>
+    /// <exception cref="ArgumentOutOfRangeException">The number of characters in the next line is larger than <see cref="int.MaxValue"/>.</exception>
+    /// <exception cref="ObjectDisposedException">The text reader has been disposed.</exception>
+    /// <exception cref="InvalidOperationException">The reader is currently in use by a previous read operation.</exception>
+    [Link("https://learn.microsoft.com/en-us/dotnet/api/system.io.textreader.readtoendasync#system-io-textreader-readlineasync(system-threading-cancellationtoken)")]
+    public static Task<string> ReadLineAsync(
+        this TextReader target,
+        CancellationToken cancellationToken)
+    {
+        cancellationToken.ThrowIfCancellationRequested();
+
+        return target.ReadToEndAsync();
+    }
 #endif
 }
