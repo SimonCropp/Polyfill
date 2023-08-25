3a1c6327db879193e2e9df74785217ef696e6edc
Martin Stühmer <3283596+samtrion@users.noreply.github.com>
2023-08-25 18:10:32 +0200

    Added `IEnumerable<TSource[]> Chunk<TSource> (this System.Collections.Generic.IEnumerable<TSource> source, int size)`, which was introduced in .NET 6

--- src/Polyfill/PolyfillExtensions_IEnumerable.cs
+++ src/Polyfill/PolyfillExtensions_IEnumerable.cs
@@ -84,6 +84,92 @@ static partial class PolyfillExtensions
 #if NETSTANDARD || NETCOREAPP || NETFRAMEWORK || NET5_0
 
     /// <summary>
+    /// Split the elements of a sequence into chunks of size at most <paramref name="size"/>.
+    /// </summary>
+    /// <remarks>
+    /// Every chunk except the last will be of size <paramref name="size"/>.
+    /// The last chunk will contain the remaining elements and may be of a smaller size.
+    /// </remarks>
+    /// <param name="source">An <see cref="IEnumerable{T}"/> whose elements to chunk.</param>
+    /// <param name="size">Maximum size of each chunk.</param>
+    /// <typeparam name="TSource">The type of the elements of source.</typeparam>
+    /// <returns>
+    /// An <see cref="IEnumerable{T}"/> that contains the elements the input sequence split into chunks of size <paramref name="size"/>.
+    /// </returns>
+    /// <exception cref="ArgumentNullException"><paramref name="source"/> is null.</exception>
+    /// <exception cref="ArgumentOutOfRangeException"><paramref name="size"/> is below 1.</exception>
+    public static IEnumerable<TSource[]> Chunk<TSource>(this IEnumerable<TSource> source, int size)
+    {
+        if (source is null)
+        {
+            throw new ArgumentNullException(nameof(source));
+        }
+
+        if (size < 1)
+        {
+            throw new ArgumentOutOfRangeException(nameof(size), size, "Size must be greater than 0.");
+        }
+
+        return ChunkIterator(source, size);
+
+        static IEnumerable<TSource[]> ChunkIterator<TSource>(IEnumerable<TSource> source, int size)
+        {
+            using IEnumerator<TSource> e = source.GetEnumerator();
+
+            // Before allocating anything, make sure there's at least one element.
+            if (e.MoveNext())
+            {
+                // Now that we know we have at least one item, allocate an initial storage array. This is not
+                // the array we'll yield.  It starts out small in order to avoid significantly overallocating
+                // when the source has many fewer elements than the chunk size.
+                int arraySize = Math.Min(size, 4);
+                int i;
+                do
+                {
+                    var array = new TSource[arraySize];
+
+                    // Store the first item.
+                    array[0] = e.Current;
+                    i = 1;
+
+                    if (size != array.Length)
+                    {
+                        // This is the first chunk. As we fill the array, grow it as needed.
+                        for (; i < size && e.MoveNext(); i++)
+                        {
+                            if (i >= array.Length)
+                            {
+                                arraySize = (int)Math.Min((uint)size, 2 * (uint)array.Length);
+                                Array.Resize(ref array, arraySize);
+                            }
+
+                            array[i] = e.Current;
+                        }
+                    }
+                    else
+                    {
+                        // For all but the first chunk, the array will already be correctly sized.
+                        // We can just store into it until either it's full or MoveNext returns false.
+                        TSource[] local = array; // avoid bounds checks by using cached local (`array` is lifted to iterator object as a field)
+                        for (; (uint)i < (uint)local.Length && e.MoveNext(); i++)
+                        {
+                            local[i] = e.Current;
+                        }
+                    }
+
+                    if (i != array.Length)
+                    {
+                        Array.Resize(ref array, i);
+                    }
+
+                    yield return array;
+                }
+                while (i >= size && e.MoveNext());
+            }
+        }
+    }
+
+    /// <summary>
     /// Returns the maximum value in a generic sequence according to a specified key selector function.
     /// </summary>
     /// <typeparam name="TSource">The type of the elements of <paramref name="target" />.</typeparam>
