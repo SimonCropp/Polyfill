c0d4bb44ba641d20b5b7f6f8eff9a90ea76818b2
Simon Cropp <simon.cropp@gmail.com>
2023-10-03 07:35:30 +1100

    .

--- src/Tests/NullabilitySync.cs
+++ src/Tests/NullabilitySync.cs
@@ -30,6 +30,12 @@ public class NullabilitySync
                 "CheckNullabilityAttributes(nullability, setter.GetParameters()[^1].GetCustomAttributesData());",
                 """
                 var parameters = setter.GetParameters();
+                CheckNullabilityAttributes(nullability, parameters[parameters.Length-1].GetCustomAttributesData());
+                """)
+            .Replace(
+                "CheckNullabilityAttributes(nullability, setter.GetParameters()[^1].GetCustomAttributesData());",
+                """
+                var parameters = setter.GetParameters();
                                 CheckNullabilityAttributes(nullability, parameters[parameters.Length-1].GetCustomAttributesData());
                 """)
             .Replace("ReadOnlySpan<ParameterInfo> parameters = metaMethod.GetParametersAsSpan();", "var parameters = metaMethod.GetParameters();")
