ee612229d6f8c0867c0eaf08181d57e66b5d9574
Scott Beca <scottbeca@mightygamesgroup.com>
2023-11-09 10:19:27 +1100

    Fix some compiler errors that can occur with specific project setups

--- src/Polyfill/Nullability/NullabilityInfo.cs
+++ src/Polyfill/Nullability/NullabilityInfo.cs
@@ -6,6 +6,7 @@
 
 using System.Linq;
 using System.Diagnostics.CodeAnalysis;
+
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
