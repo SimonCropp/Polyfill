fd4b85f59cd865a253f28d5aaab2d10f960864e0
Scott Beca <sbeca@users.noreply.github.com>
2023-11-08 13:26:07 +1100

    Add more docs and tests (#100)

--- src/Polyfill/PolyfillExtensions_Task.cs
+++ src/Polyfill/PolyfillExtensions_Task.cs
@@ -12,80 +12,116 @@ using Link = System.ComponentModel.DescriptionAttribute;
 
 static partial class PolyfillExtensions
 {
+    // Copied from .NET library const Timer.MaxSupportedTimeout
+    private const uint MaxSupportedTimeout = 0xfffffffe;
+
+    /// <summary>Gets a <see cref="Task"/> that will complete when this <see cref="Task"/> completes or when the specified <see cref="CancellationToken"/> has cancellation requested.</summary>
+    /// <param name="cancellationToken">The <see cref="CancellationToken"/> to monitor for a cancellation request.</param>
+    /// <returns>The <see cref="Task"/> representing the asynchronous wait.  It may or may not be the same instance as the current instance.</returns>
     [Link("https://learn.microsoft.com/en-us/dotnet/api/system.threading.tasks.task.waitasync#system-threading-tasks-task-waitasync(system-threading-cancellationtoken)")]
     public static Task WaitAsync(this Task target, CancellationToken cancellationToken) =>
         target.WaitAsync(Timeout.InfiniteTimeSpan, cancellationToken);
 
+    /// <summary>Gets a <see cref="Task"/> that will complete when this <see cref="Task"/> completes or when the specified timeout expires.</summary>
+    /// <param name="timeout">The timeout after which the <see cref="Task"/> should be faulted with a <see cref="TimeoutException"/> if it hasn't otherwise completed.</param>
+    /// <returns>The <see cref="Task"/> representing the asynchronous wait.  It may or may not be the same instance as the current instance.</returns>
     [Link("https://learn.microsoft.com/en-us/dotnet/api/system.threading.tasks.task.waitasync#system-threading-tasks-task-waitasync(system-timespan)")]
-    public static async Task WaitAsync(
+    public static Task WaitAsync(
         this Task target,
-        TimeSpan timeout)
-    {
-        var cancelSource = new CancellationTokenSource();
-        try
-        {
-            await target.WaitAsync(timeout, cancelSource.Token);
-        }
-        finally
-        {
-            cancelSource.Cancel();
-            cancelSource.Dispose();
-        }
-    }
+        TimeSpan timeout) =>
+        target.WaitAsync(timeout, default);
 
+    /// <summary>Gets a <see cref="Task"/> that will complete when this <see cref="Task"/> completes, when the specified timeout expires, or when the specified <see cref="CancellationToken"/> has cancellation requested.</summary>
+    /// <param name="timeout">The timeout after which the <see cref="Task"/> should be faulted with a <see cref="TimeoutException"/> if it hasn't otherwise completed.</param>
+    /// <param name="cancellationToken">The <see cref="CancellationToken"/> to monitor for a cancellation request.</param>
+    /// <returns>The <see cref="Task"/> representing the asynchronous wait.  It may or may not be the same instance as the current instance.</returns>
     [Link("https://learn.microsoft.com/en-us/dotnet/api/system.threading.tasks.task.waitasync#system-threading-tasks-task-waitasync(system-timespan-system-threading-cancellationtoken)")]
-    public static async Task WaitAsync(
+    public static Task WaitAsync(
         this Task target,
         TimeSpan timeout,
         CancellationToken cancellationToken)
     {
-        var delayTask = Task.Delay(timeout, cancellationToken);
-        var completedTask = await Task.WhenAny(target, delayTask);
-        if (completedTask == delayTask)
+        if (target is null)
         {
-            throw new TimeoutException($"Execution did not complete within the time allotted {timeout.TotalMilliseconds} ms");
+            throw new ArgumentNullException(nameof(target));
         }
 
-        await target;
+        long totalMilliseconds = (long)timeout.TotalMilliseconds;
+        if (totalMilliseconds < -1 || totalMilliseconds > MaxSupportedTimeout)
+        {
+            throw new ArgumentOutOfRangeException(nameof(timeout));
+        }
+
+        if (target.IsCompleted || (!cancellationToken.CanBeCanceled && timeout == Timeout.InfiniteTimeSpan))
+        {
+            return target;
+        }
+
+        if (cancellationToken.IsCancellationRequested)
+        {
+            return Task.FromCanceled(cancellationToken);
+        }
+
+        if (timeout == TimeSpan.Zero)
+        {
+            return Task.FromException(new TimeoutException());
+        }
+
+        var delayTask = Task.Delay(timeout, cancellationToken);
+
+        Func<Task<Task>, Task> continueFunc = completedTask =>
+        {
+            if (cancellationToken.IsCancellationRequested)
+            {
+                throw new TaskCanceledException();
+            }
+            else if (completedTask.Result == delayTask)
+            {
+                throw new TimeoutException($"Execution did not complete within the time allotted {timeout.TotalMilliseconds} ms");
+            }
+
+            return target;
+        };
+
+        return Task.WhenAny(target, delayTask).ContinueWith(continueFunc, TaskContinuationOptions.OnlyOnRanToCompletion);
     }
 
+    /// <summary>
+    /// Gets a <see cref="Task"/> that will complete when this <see cref="Task"/> completes, or when the specified <see cref="CancellationToken"/> has cancellation requested.
+    /// </summary>
+    /// <param name="cancellationToken">The <see cref="CancellationToken"/> to monitor for a cancellation request.</param>
+    /// <returns>The <see cref="Task"/> representing the asynchronous wait.  It may or may not be the same instance as the current instance.</returns>
     [Link("https://learn.microsoft.com/en-us/dotnet/api/system.threading.tasks.task.waitasync#system-threading-tasks-task-waitasync(system-threading-cancellationtoken)")]
     public static Task<TResult> WaitAsync<TResult>(
         this Task<TResult> target,
         CancellationToken cancellationToken) =>
         target.WaitAsync<TResult>(Timeout.InfiniteTimeSpan, cancellationToken);
 
-    [Link("https://learn.microsoft.com/en-us/dotnet/api/system.threading.tasks.task-1.waitasync#system-threading-tasks-task-1-waitasync(system-threading-cancellationtoken)")]
-    public static async Task<TResult> WaitAsync<TResult>(
+    /// <summary>
+    /// Gets a <see cref="Task"/> that will complete when this <see cref="Task"/> completes, or when the specified timeout expires.
+    /// </summary>
+    /// <param name="timeout">The timeout after which the <see cref="Task"/> should be faulted with a <see cref="TimeoutException"/> if it hasn't otherwise completed.</param>
+    /// <returns>The <see cref="Task"/> representing the asynchronous wait.  It may or may not be the same instance as the current instance.</returns>
+    [Link("https://learn.microsoft.com/en-us/dotnet/api/system.threading.tasks.task-1.waitasync#system-threading-tasks-task-1-waitasync(system-timespan)")]
+    public static Task<TResult> WaitAsync<TResult>(
         this Task<TResult> target,
-        TimeSpan timeout)
-    {
-        var cancelSource = new CancellationTokenSource();
-        try
-        {
-            return await target.WaitAsync(timeout, cancelSource.Token);
-        }
-        finally
-        {
-            cancelSource.Cancel();
-            cancelSource.Dispose();
-        }
-    }
+        TimeSpan timeout) =>
+        target.WaitAsync<TResult>(timeout, default);
 
+    /// <summary>
+    /// Gets a <see cref="Task"/> that will complete when this <see cref="Task"/> completes, when the specified timeout expires, or when the specified <see cref="CancellationToken"/> has cancellation requested.
+    /// </summary>
+    /// <param name="timeout">The timeout after which the <see cref="Task"/> should be faulted with a <see cref="TimeoutException"/> if it hasn't otherwise completed.</param>
+    /// <param name="cancellationToken">The <see cref="CancellationToken"/> to monitor for a cancellation request.</param>
+    /// <returns>The <see cref="Task"/> representing the asynchronous wait.  It may or may not be the same instance as the current instance.</returns>
     [Link("https://learn.microsoft.com/en-us/dotnet/api/system.threading.tasks.task-1.waitasync#system-threading-tasks-task-1-waitasync(system-timespan-system-threading-cancellationtoken)")]
     public static async Task<TResult> WaitAsync<TResult>(
         this Task<TResult> target,
         TimeSpan timeout,
         CancellationToken cancellationToken)
     {
-        var delayTask = Task.Delay(timeout, cancellationToken);
-        var completedTask = await Task.WhenAny(target, delayTask);
-        if (completedTask == delayTask)
-        {
-            throw new TimeoutException($"Execution did not complete within the time allotted {timeout.TotalMilliseconds} ms");
-        }
-
-        return await target;
+        await ((Task)target).WaitAsync(timeout, cancellationToken);
+        return target.Result;
     }
 }
 
--- src/Tests/PolyfillExtensionsTests_Memory.cs
+++ src/Tests/PolyfillExtensionsTests_Memory.cs
@@ -1,15 +1,245 @@
 partial class PolyfillExtensionsTests
 {
     [Test]
-    public void SpanContains()
+    public void ReadOnlySpan_ZeroLengthContains()
     {
-        Assert.True("value".AsSpan().Contains('e'));
-        var span = new Span<char>("value".ToCharArray());
-        Assert.True(span.Contains('e'));
+        ReadOnlySpan<int> span = new ReadOnlySpan<int>(Array.Empty<int>());
+
+        bool found = span.Contains(0);
+        Assert.False(found);
     }
 
     [Test]
-    public void SpanSequenceEqual()
+    public void ReadOnlySpan_TestContains()
+    {
+        for (int length = 0; length < 32; length++)
+        {
+            int[] a = new int[length];
+            for (int i = 0; i < length; i++)
+            {
+                a[i] = 10 * (i + 1);
+            }
+            ReadOnlySpan<int> span = new ReadOnlySpan<int>(a);
+
+            for (int targetIndex = 0; targetIndex < length; targetIndex++)
+            {
+                int target = a[targetIndex];
+                bool found = span.Contains(target);
+                Assert.True(found);
+            }
+        }
+    }
+
+    [Test]
+    public void ReadOnlySpan_TestMultipleContains()
+    {
+        for (int length = 2; length < 32; length++)
+        {
+            int[] a = new int[length];
+            for (int i = 0; i < length; i++)
+            {
+                a[i] = 10 * (i + 1);
+            }
+
+            a[length - 1] = 5555;
+            a[length - 2] = 5555;
+
+            ReadOnlySpan<int> span = new ReadOnlySpan<int>(a);
+            bool found = span.Contains(5555);
+            Assert.True(found);
+        }
+    }
+
+    [Test]
+    public void ReadOnlySpan_ZeroLengthContains_String()
+    {
+        ReadOnlySpan<string> span = new ReadOnlySpan<string>(Array.Empty<string>());
+        bool found = span.Contains("a");
+        Assert.False(found);
+    }
+
+    [Test]
+    public void ReadOnlySpan_TestMatchContains_String()
+    {
+        for (int length = 0; length < 32; length++)
+        {
+            string[] a = new string[length];
+            for (int i = 0; i < length; i++)
+            {
+                a[i] = (10 * (i + 1)).ToString();
+            }
+            ReadOnlySpan<string> span = new ReadOnlySpan<string>(a);
+
+            for (int targetIndex = 0; targetIndex < length; targetIndex++)
+            {
+                string target = a[targetIndex];
+                bool found = span.Contains(target);
+                Assert.True(found);
+            }
+        }
+    }
+
+    [Test]
+    public void ReadOnlySpan_TestNoMatchContains_String()
+    {
+        var rnd = new Random(42);
+        for (int length = 0; length <= byte.MaxValue; length++)
+        {
+            string[] a = new string[length];
+            string target = (rnd.Next(0, 256)).ToString();
+            for (int i = 0; i < length; i++)
+            {
+                string val = (i + 1).ToString();
+                a[i] = val == target ? (target + 1) : val;
+            }
+            ReadOnlySpan<string> span = new ReadOnlySpan<string>(a);
+
+            bool found = span.Contains(target);
+            Assert.False(found);
+        }
+    }
+
+    [Test]
+    public void ReadOnlySpan_TestMultipleMatchContains_String()
+    {
+        for (int length = 2; length < 32; length++)
+        {
+            string[] a = new string[length];
+            for (int i = 0; i < length; i++)
+            {
+                a[i] = (10 * (i + 1)).ToString();
+            }
+
+            a[length - 1] = "5555";
+            a[length - 2] = "5555";
+
+            ReadOnlySpan<string> span = new ReadOnlySpan<string>(a);
+            bool found = span.Contains("5555");
+            Assert.True(found);
+        }
+    }
+
+    [Test]
+    public void Span_ZeroLengthContains()
+    {
+        Span<int> span = new Span<int>(Array.Empty<int>());
+
+        bool found = span.Contains(0);
+        Assert.False(found);
+    }
+
+    [Test]
+    public void Span_TestContains()
+    {
+        for (int length = 0; length < 32; length++)
+        {
+            int[] a = new int[length];
+            for (int i = 0; i < length; i++)
+            {
+                a[i] = 10 * (i + 1);
+            }
+            Span<int> span = new Span<int>(a);
+
+            for (int targetIndex = 0; targetIndex < length; targetIndex++)
+            {
+                int target = a[targetIndex];
+                bool found = span.Contains(target);
+                Assert.True(found);
+            }
+        }
+    }
+
+    [Test]
+    public void Span_TestMultipleContains()
+    {
+        for (int length = 2; length < 32; length++)
+        {
+            int[] a = new int[length];
+            for (int i = 0; i < length; i++)
+            {
+                a[i] = 10 * (i + 1);
+            }
+
+            a[length - 1] = 5555;
+            a[length - 2] = 5555;
+
+            Span<int> span = new Span<int>(a);
+            bool found = span.Contains(5555);
+            Assert.True(found);
+        }
+    }
+
+    [Test]
+    public void Span_ZeroLengthContains_String()
+    {
+        Span<string> span = new Span<string>(Array.Empty<string>());
+        bool found = span.Contains("a");
+        Assert.False(found);
+    }
+
+    [Test]
+    public void Span_TestMatchContains_String()
+    {
+        for (int length = 0; length < 32; length++)
+        {
+            string[] a = new string[length];
+            for (int i = 0; i < length; i++)
+            {
+                a[i] = (10 * (i + 1)).ToString();
+            }
+            Span<string> span = new Span<string>(a);
+
+            for (int targetIndex = 0; targetIndex < length; targetIndex++)
+            {
+                string target = a[targetIndex];
+                bool found = span.Contains(target);
+                Assert.True(found);
+            }
+        }
+    }
+
+    [Test]
+    public void Span_TestNoMatchContains_String()
+    {
+        var rnd = new Random(42);
+        for (int length = 0; length <= byte.MaxValue; length++)
+        {
+            string[] a = new string[length];
+            string target = (rnd.Next(0, 256)).ToString();
+            for (int i = 0; i < length; i++)
+            {
+                string val = (i + 1).ToString();
+                a[i] = val == target ? (target + 1) : val;
+            }
+            Span<string> span = new Span<string>(a);
+
+            bool found = span.Contains(target);
+            Assert.False(found);
+        }
+    }
+
+    [Test]
+    public void Span_TestMultipleMatchContains_String()
+    {
+        for (int length = 2; length < 32; length++)
+        {
+            string[] a = new string[length];
+            for (int i = 0; i < length; i++)
+            {
+                a[i] = (10 * (i + 1)).ToString();
+            }
+
+            a[length - 1] = "5555";
+            a[length - 2] = "5555";
+
+            Span<string> span = new Span<string>(a);
+            bool found = span.Contains("5555");
+            Assert.True(found);
+        }
+    }
+
+    [Test]
+    public void Span_SequenceEqual()
     {
         Assert.True("value".AsSpan().SequenceEqual("value"));
         Assert.False("value".AsSpan().SequenceEqual("value2"));
@@ -21,7 +251,7 @@ partial class PolyfillExtensionsTests
     }
 
     [Test]
-    public void SpanStartsWith()
+    public void Span_StartsWith()
     {
         Assert.True("value".AsSpan().StartsWith("value"));
         Assert.False("value".AsSpan().StartsWith("value2"));
@@ -33,7 +263,7 @@ partial class PolyfillExtensionsTests
     }
 
     [Test]
-    public void SpanEndsWith()
+    public void Span_EndsWith()
     {
         Assert.True("value".AsSpan().EndsWith("value"));
         Assert.False("value".AsSpan().EndsWith("value2"));
