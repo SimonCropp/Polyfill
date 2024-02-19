5635e3fd6ef423066bd8ca4e0d778ac4bb8bf104
Simon Cropp <simon.cropp@gmail.com>
2024-02-19 21:18:24 +1100

    Update Polyfill_StringBuilder.cs

--- src/Polyfill/Polyfill_StringBuilder.cs
+++ src/Polyfill/Polyfill_StringBuilder.cs
@@ -7,8 +7,6 @@ using System.IO;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 using System.Text;
-using System.Threading;
-using System.Threading.Tasks;
 using Link = System.ComponentModel.DescriptionAttribute;
 
 static partial class Polyfill
