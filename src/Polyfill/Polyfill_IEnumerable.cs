ae8023244192e8538d4e5ed54f773d229250668f
Simon Cropp <simon.cropp@gmail.com>
2024-03-30 22:34:09 +1100

    .

--- src/Polyfill/Polyfill_IEnumerable.cs
+++ src/Polyfill/Polyfill_IEnumerable.cs
@@ -4,6 +4,7 @@
 
 using System;
 using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 using Link = System.ComponentModel.DescriptionAttribute;
 using System.Linq;
 
@@ -261,68 +262,50 @@ static partial class Polyfill
             .OrderBy(keySelector, comparer)
             .FirstOrDefault();
 
+    /// <summary>Returns distinct elements from a sequence according to a specified key selector function.</summary>
+    /// <typeparam name="TSource">The type of the elements of <paramref name="source" />.</typeparam>
+    /// <typeparam name="TKey">The type of key to distinguish elements by.</typeparam>
+    /// <param name="source">The sequence to remove duplicate elements from.</param>
+    /// <param name="keySelector">A function to extract the key for each element.</param>
+    /// <returns>An <see cref="IEnumerable{T}" /> that contains distinct elements from the source sequence.</returns>
+    /// <exception cref="ArgumentNullException"><paramref name="source" /> is <see langword="null" />.</exception>
+    /// <remarks>
+    /// <para>This method is implemented by using deferred execution. The immediate return value is an object that stores all the information that is required to perform the action. The query represented by this method is not executed until the object is enumerated either by calling its `GetEnumerator` method directly or by using `foreach` in Visual C# or `For Each` in Visual Basic.</para>
+    /// <para>The <see cref="DistinctBy{TSource, TKey}(IEnumerable{TSource}, Func{TSource, TKey})" /> method returns an unordered sequence that contains no duplicate values. The default equality comparer, <see cref="EqualityComparer{T}.Default" />, is used to compare values.</para>
+    /// </remarks>
     public static IEnumerable<TSource> DistinctBy<TSource, TKey>(this IEnumerable<TSource> source, Func<TSource, TKey> keySelector) =>
-        throw new NotImplementedException();
+        DistinctBy(source, keySelector, null);
 
-    public static IEnumerable<TSource> DistinctBy<TSource, TKey>(this IEnumerable<TSource> source, Func<TSource, TKey> keySelector, IEqualityComparer<TKey> comparer) =>
-        throw new NotImplementedException();
+    /// <summary>Returns distinct elements from a sequence according to a specified key selector function.</summary>
+    /// <typeparam name="TSource">The type of the elements of <paramref name="source" />.</typeparam>
+    /// <typeparam name="TKey">The type of key to distinguish elements by.</typeparam>
+    /// <param name="source">The sequence to remove duplicate elements from.</param>
+    /// <param name="keySelector">A function to extract the key for each element.</param>
+    /// <param name="comparer">An <see cref="IEqualityComparer{TKey}" /> to compare keys.</param>
+    /// <returns>An <see cref="IEnumerable{T}" /> that contains distinct elements from the source sequence.</returns>
+    /// <exception cref="ArgumentNullException"><paramref name="source" /> is <see langword="null" />.</exception>
+    /// <remarks>
+    /// <para>This method is implemented by using deferred execution. The immediate return value is an object that stores all the information that is required to perform the action. The query represented by this method is not executed until the object is enumerated either by calling its `GetEnumerator` method directly or by using `foreach` in Visual C# or `For Each` in Visual Basic.</para>
+    /// <para>The <see cref="DistinctBy{TSource, TKey}(IEnumerable{TSource}, Func{TSource, TKey}, IEqualityComparer{TKey}?)" /> method returns an unordered sequence that contains no duplicate values. If <paramref name="comparer" /> is <see langword="null" />, the default equality comparer, <see cref="EqualityComparer{T}.Default" />, is used to compare values.</para>
+    /// </remarks>
+    public static IEnumerable<TSource> DistinctBy<TSource, TKey>(this IEnumerable<TSource> source, Func<TSource, TKey> keySelector, IEqualityComparer<TKey> comparer)
+    {
+        using IEnumerator<TSource> enumerator = source.GetEnumerator();
 
-    public static TSource ElementAt<TSource>(this IEnumerable<TSource> source, Index index) =>
-        throw new NotImplementedException();
-
-    public static TSource ElementAtOrDefault<TSource>(this IEnumerable<TSource> source, Index index) =>
-        throw new NotImplementedException();
-
-    public static IEnumerable<TSource> ExceptBy<TSource, TKey>(this IEnumerable<TSource> first, IEnumerable<TKey> second, Func<TSource, TKey> keySelector) =>
-        throw new NotImplementedException();
-
-    public static IEnumerable<TSource> ExceptBy<TSource, TKey>(this IEnumerable<TSource> first, IEnumerable<TKey> second, Func<TSource, TKey> keySelector, IEqualityComparer<TKey> comparer) =>
-        throw new NotImplementedException();
-
-    public static TSource FirstOrDefault<TSource>(this IEnumerable<TSource> source, Func<TSource, bool> predicate, TSource defaultValue) =>
-        throw new NotImplementedException();
-
-    public static TSource FirstOrDefault<TSource>(this IEnumerable<TSource> source, TSource defaultValue) =>
-        throw new NotImplementedException();
-
-    public static IEnumerable<TSource> IntersectBy<TSource, TKey>(this IEnumerable<TSource> first, IEnumerable<TKey> second, Func<TSource, TKey> keySelector) =>
-        throw new NotImplementedException();
-
-    public static IEnumerable<TSource> IntersectBy<TSource, TKey>(this IEnumerable<TSource> first, IEnumerable<TKey> second, Func<TSource, TKey> keySelector, IEqualityComparer<TKey> comparer) =>
-        throw new NotImplementedException();
-
-    public static TSource LastOrDefault<TSource>(this IEnumerable<TSource> source, Func<TSource, bool> predicate, TSource defaultValue) =>
-        throw new NotImplementedException();
-
-    public static TSource LastOrDefault<TSource>(this IEnumerable<TSource> source, TSource defaultValue) =>
-        throw new NotImplementedException();
-
-    public static TSource Max<TSource>(this IEnumerable<TSource> source, IComparer<TSource> comparer) =>
-        throw new NotImplementedException();
-
-    public static TSource Min<TSource>(this IEnumerable<TSource> source, IComparer<TSource> comparer) =>
-        throw new NotImplementedException();
-
-    public static TSource SingleOrDefault<TSource>(this IEnumerable<TSource> source, Func<TSource, bool> predicate, TSource defaultValue) =>
-        throw new NotImplementedException();
-
-    public static TSource SingleOrDefault<TSource>(this IEnumerable<TSource> source, TSource defaultValue) =>
-        throw new NotImplementedException();
-
-    public static IEnumerable<TSource> Take<TSource>(this IEnumerable<TSource> source, Range range) =>
-        throw new NotImplementedException();
-
-    public static bool TryGetNonEnumeratedCount<TSource>(this IEnumerable<TSource> source, out int count) =>
-        throw new NotImplementedException();
-
-    public static IEnumerable<TSource> UnionBy<TSource, TKey>(this IEnumerable<TSource> first, IEnumerable<TSource> second, Func<TSource, TKey> keySelector) =>
-        throw new NotImplementedException();
-
-    public static IEnumerable<TSource> UnionBy<TSource, TKey>(this IEnumerable<TSource> first, IEnumerable<TSource> second, Func<TSource, TKey> keySelector, IEqualityComparer<TKey> comparer) =>
-        throw new NotImplementedException();
-
-    public static IEnumerable<ValueTuple<TFirst, TSecond, TThird>> Zip<TFirst, TSecond, TThird>(this IEnumerable<TFirst> first, IEnumerable<TSecond> second, IEnumerable<TThird> third) =>
-        throw new NotImplementedException();
+        if (enumerator.MoveNext())
+        {
+            var set = new HashSet<TKey>(comparer);
+            do
+            {
+                TSource element = enumerator.Current;
+                if (set.Add(keySelector(element)))
+                {
+                    yield return element;
+                }
+            }
+            while (enumerator.MoveNext());
+        }
+    }
 
 #endif
 
