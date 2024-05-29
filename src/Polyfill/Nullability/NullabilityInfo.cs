74c058e32388a32087abcbb9474eeaa71be78b48
Simon Cropp <simon.cropp@gmail.com>
2024-05-29 13:32:12 +1000

    remove NullabilitySync

--- src/Polyfill/Nullability/NullabilityInfo.cs
+++ src/Polyfill/Nullability/NullabilityInfo.cs
@@ -12,77 +12,75 @@ using System.Diagnostics.CodeAnalysis;
 
 using System.Collections.ObjectModel;
 
-namespace System.Reflection
-{
-    using System.Linq;
-    using System.Diagnostics.CodeAnalysis;
-    using System.Collections.Generic;
-    using System.Collections.ObjectModel;
-    using System.Diagnostics;
+namespace System.Reflection;
 
-    /// <summary>
-    /// A class that represents nullability info
-    /// </summary>
-    [ExcludeFromCodeCoverage]
+using System.Linq;
+using System.Diagnostics.CodeAnalysis;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+
+/// <summary>
+/// A class that represents nullability info
+/// </summary>
+[ExcludeFromCodeCoverage]
 #if PolyPublic
 public
 #endif
 sealed class NullabilityInfo
+{
+    internal NullabilityInfo(Type type, NullabilityState readState, NullabilityState writeState, NullabilityInfo? elementType, NullabilityInfo[] typeArguments)
     {
-        internal NullabilityInfo(Type type, NullabilityState readState, NullabilityState writeState,
-            NullabilityInfo? elementType, NullabilityInfo[] typeArguments)
-        {
-            Type = type;
-            ReadState = readState;
-            WriteState = writeState;
-            ElementType = elementType;
-            GenericTypeArguments = typeArguments;
-        }
-
-        /// <summary>
-        /// The <see cref="System.Type" /> of the member or generic parameter
-        /// to which this NullabilityInfo belongs
-        /// </summary>
-        public Type Type { get; }
-        /// <summary>
-        /// The nullability read state of the member
-        /// </summary>
-        public NullabilityState ReadState { get; internal set; }
-        /// <summary>
-        /// The nullability write state of the member
-        /// </summary>
-        public NullabilityState WriteState { get; internal set; }
-        /// <summary>
-        /// If the member type is an array, gives the <see cref="NullabilityInfo" /> of the elements of the array, null otherwise
-        /// </summary>
-        public NullabilityInfo? ElementType { get; }
-        /// <summary>
-        /// If the member type is a generic type, gives the array of <see cref="NullabilityInfo" /> for each type parameter
-        /// </summary>
-        public NullabilityInfo[] GenericTypeArguments { get; }
+        Type = type;
+        ReadState = readState;
+        WriteState = writeState;
+        ElementType = elementType;
+        GenericTypeArguments = typeArguments;
     }
 
     /// <summary>
-    /// An enum that represents nullability state
+    /// The <see cref="System.Type" /> of the member or generic parameter
+    /// to which this NullabilityInfo belongs
     /// </summary>
-    #if PolyPublic
+    public Type Type { get; }
+    /// <summary>
+    /// The nullability read state of the member
+    /// </summary>
+    public NullabilityState ReadState { get; internal set; }
+    /// <summary>
+    /// The nullability write state of the member
+    /// </summary>
+    public NullabilityState WriteState { get; internal set; }
+    /// <summary>
+    /// If the member type is an array, gives the <see cref="NullabilityInfo" /> of the elements of the array, null otherwise
+    /// </summary>
+    public NullabilityInfo? ElementType { get; }
+    /// <summary>
+    /// If the member type is a generic type, gives the array of <see cref="NullabilityInfo" /> for each type parameter
+    /// </summary>
+    public NullabilityInfo[] GenericTypeArguments { get; }
+}
+
+/// <summary>
+/// An enum that represents nullability state
+/// </summary>
+#if PolyPublic
 public
 #endif
 enum NullabilityState
-    {
-        /// <summary>
-        /// Nullability context not enabled (oblivious)
-        /// </summary>
-        Unknown,
-        /// <summary>
-        /// Non nullable value or reference type
-        /// </summary>
-        NotNull,
-        /// <summary>
-        /// Nullable value or reference type
-        /// </summary>
-        Nullable
-    }
+{
+    /// <summary>
+    /// Nullability context not enabled (oblivious)
+    /// </summary>
+    Unknown,
+    /// <summary>
+    /// Non nullable value or reference type
+    /// </summary>
+    NotNull,
+    /// <summary>
+    /// Nullable value or reference type
+    /// </summary>
+    Nullable
 }
 
 #endif
