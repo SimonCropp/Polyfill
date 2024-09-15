b4a44d1ceab2c9c554b34b05b89a04a586cedba4
Keith Dahlby <dahlbyk@gmail.com>
2024-09-15 18:56:25 -0500

    Always check NET8_0 (#225)

--- src/Polyfill/Polyfill_Memory_SpanSplitEnumerator.cs
+++ src/Polyfill/Polyfill_Memory_SpanSplitEnumerator.cs
@@ -198,14 +198,11 @@ static partial class Polyfill
 
         const string whitespaces = "\t\n\v\f\r\u0020\u0085\u00a0\u1680\u2000\u2001\u2002\u2003\u2004\u2005\u2006\u2007\u2008\u2009\u200a\u2028\u2029\u202f\u205f\u3000";
 
-#if NET8_0_OR_GREATER
+#if NET8_0
         public static readonly SearchValues<char> WhiteSpaceChars =
             SearchValues.Create(whitespaces.AsSpan());
 #else
         public static readonly T[] WhiteSpaceChars;
-#endif
-
-#if !NET8_0
 
         static SpanSplitEnumerator()
         {
@@ -216,7 +213,6 @@ static partial class Polyfill
                     .ToArray();
             }
         }
-
 #endif
     }
 }
