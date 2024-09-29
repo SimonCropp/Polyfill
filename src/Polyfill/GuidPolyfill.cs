24c930ecb028f6cbfe4bbd8e4bfea2ec8b227114
Simon Cropp <simon.cropp@gmail.com>
2024-09-29 18:41:20 +1000

    Update GuidPolyfill.cs

--- src/Polyfill/GuidPolyfill.cs
+++ src/Polyfill/GuidPolyfill.cs
@@ -22,10 +22,10 @@ static partial class GuidPolyfill
     /// </summary>
     [Link("https://learn.microsoft.com/en-us/dotnet/api/system.guid.tryparse#system-guid-tryparse(system-string-system-iformatprovider-system-guid@)")]
     public static bool TryParse(string? target, IFormatProvider? provider, out Guid result) =>
-#if !NET7_0_OR_GREATER
-        Guid.TryParse(target, out result);
-#else
+#if NET7_0_OR_GREATER
         Guid.TryParse(target, provider, out result);
+#else
+        Guid.TryParse(target, out result);
 #endif
 
 #if FeatureMemory
@@ -34,10 +34,10 @@ static partial class GuidPolyfill
     /// </summary>
     [Link("https://learn.microsoft.com/en-us/dotnet/api/system.byte.tryparse#system-byte-tryparse(system-readonlyspan((system-byte))-system-iformatprovider-system-byte@)")]
     public static bool TryParse(ReadOnlySpan<byte> target, IFormatProvider? provider, out byte result) =>
-#if !NET8_0_OR_GREATER
-        byte.TryParse(Encoding.UTF8.GetString(target.ToArray()), NumberStyles.Integer, provider, out result);
-#else
+#if NET8_0_OR_GREATER
         byte.TryParse(target, provider, out result);
+#else
+        byte.TryParse(Encoding.UTF8.GetString(target.ToArray()), NumberStyles.Integer, provider, out result);
 #endif
 
 #endif
