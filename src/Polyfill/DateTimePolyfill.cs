3604c06337eb56a22562f207cbec0c401882f38d
Simon Cropp <simon.cropp@gmail.com>
2024-10-03 18:33:04 +1000

    add more DateTime.TryParseExact overloads

--- src/Polyfill/DateTimePolyfill.cs
+++ src/Polyfill/DateTimePolyfill.cs
@@ -66,11 +66,22 @@ static partial class DateTimePolyfill
     /// Tries to parse a span of characters into a value.
     /// </summary>
     [Link("https://learn.microsoft.com/en-us/dotnet/api/system.datetime.tryparseexact#system-datetime-tryparseexact(system-readonlyspan((system-char))-system-readonlyspan((system-char))-system-iformatprovider-system-globalization-datetimestyles-system-datetime@)")]
-    public static bool TryParseExact(ReadOnlySpan<char> target, string format, IFormatProvider? provider, DateTimeStyles styles, out DateTime result) =>
+    public static bool TryParseExact(ReadOnlySpan<char> target, string format, IFormatProvider? provider, DateTimeStyles style, out DateTime result) =>
 #if NETSTANDARD2_1 || NETSTANDARD2_1_OR_GREATER || NET5_0_OR_GREATER
-        DateTime.TryParseExact(target, format, provider, styles, out result);
+        DateTime.TryParseExact(target, format, provider, style, out result);
 #else
-        DateTime.TryParseExact(target.ToString(), format, provider, styles, out result);
+        DateTime.TryParseExact(target.ToString(), format, provider, style, out result);
+#endif
+
+    /// <summary>
+    /// Tries to parse a span of characters into a value.
+    /// </summary>
+    [Link("https://learn.microsoft.com/en-us/dotnet/api/system.datetime.tryparseexact#system-datetime-tryparseexact(system-readonlyspan((system-char))-system-readonlyspan((system-char))-system-iformatprovider-system-globalization-datetimestyles-system-datetime@)")]
+    public static bool TryParseExact(ReadOnlySpan<char> target, ReadOnlySpan<char> format, IFormatProvider? provider, DateTimeStyles styles, out DateTime result) =>
+#if NETFRAMEWORK || NETSTANDARD2_0 || NETCOREAPP2_0
+        DateTime.TryParseExact(target.ToString(), format.ToString(), provider, styles, out result);
+#else
+        DateTime.TryParseExact(target, format, provider, styles, out result);
 #endif
 
 #endif
