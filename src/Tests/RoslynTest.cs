3d77107cdc93016e2ce16e17b57485cbad16a08e
Simon Cropp <simon.cropp@gmail.com>
2024-10-08 15:07:12 +1100

    RoslynTest

---
+++ src/Tests/RoslynTest.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+#if NET9_0
+[TestFixture]
+public class RoslynTest
+{
+    [Test]
+    public void Run()
+    {
+        var solutionDirectory = SolutionDirectoryFinder.Find();
+        var polyfillPath = Path.Combine(solutionDirectory, "Polyfill");
+        var options = new CSharpParseOptions(LanguageVersion.CSharp12).WithPreprocessorSymbols();
+        foreach (var file in Directory.EnumerateFiles(polyfillPath, "*.cs", SearchOption.AllDirectories))
+        {
+            var tree = CSharpSyntaxTree.ParseText(File.ReadAllText(file), options);
+            var root = tree.GetRoot();
+
+            var usings = root.DescendantNodes();
+            foreach (var node in usings)
+            {
+                Debug.WriteLine(node.GetType());
+            }
+        }
+    }
+}
+#endif
