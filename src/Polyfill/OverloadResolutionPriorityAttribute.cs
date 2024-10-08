6d4e0b4dc9167fa372634805987f76d40780362a
Simon Cropp <simon.cropp@gmail.com>
2024-10-08 22:15:45 +1100

    Update OverloadResolutionPriorityAttribute.cs

--- src/Polyfill/OverloadResolutionPriorityAttribute.cs
+++ src/Polyfill/OverloadResolutionPriorityAttribute.cs
@@ -5,9 +5,9 @@
 
 namespace System.Runtime.CompilerServices;
 
-using System.Diagnostics;
-using System.Diagnostics.CodeAnalysis;
-using Link = System.ComponentModel.DescriptionAttribute;
+using Diagnostics;
+using Diagnostics.CodeAnalysis;
+using Link = ComponentModel.DescriptionAttribute;
 
 /// <summary>
 /// Specifies the priority of a member in overload resolution. When unspecified, the default priority is 0.
@@ -19,6 +19,7 @@ using Link = System.ComponentModel.DescriptionAttribute;
     AttributeTargets.Constructor |
     AttributeTargets.Property,
     Inherited = false)]
+[Link("https://learn.microsoft.com/en-us/dotnet/api/system.runtime.compilerservices.overloadresolutionpriorityattribute")]
 #if PolyPublic
 public
 #endif
