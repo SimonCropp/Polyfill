adac589f249dac46d588e8b116b55fc566e72ea4
Simon Cropp <simon.cropp@gmail.com>
2024-10-08 22:10:01 +1100

    Update GuidPolyfill.cs

--- src/Polyfill/GuidPolyfill.cs
+++ src/Polyfill/GuidPolyfill.cs
@@ -3,11 +3,8 @@
 
 namespace Polyfills;
 using System;
-using System.Collections.Generic;
 using System.Diagnostics;
 using System.Diagnostics.CodeAnalysis;
-using System.Globalization;
-using System.Text;
 using Link = System.ComponentModel.DescriptionAttribute;
 
 [ExcludeFromCodeCoverage]
@@ -15,7 +12,7 @@ using Link = System.ComponentModel.DescriptionAttribute;
 #if PolyPublic
 public
 #endif
-static partial class GuidPolyfill
+static class GuidPolyfill
 {
     /// <summary>
     /// Tries to parse a string into a value.
