ba7ce0589d21b77cdf7b81b7d626b4a0a4b2a7c8
Simon Cropp <simon.cropp@gmail.com>
2024-10-10 21:15:16 +1100

    .

--- src/Tests/RoslynTests/CommentStripper.cs
+++ src/Tests/RoslynTests/CommentStripper.cs
@@ -1,15 +1,6 @@
 ﻿#if NET9_0
-using Microsoft.CodeAnalysis;
-using Microsoft.CodeAnalysis.CSharp;
-using Microsoft.CodeAnalysis.CSharp.Syntax;
-using Microsoft.CodeAnalysis.Text;
-
 class CommentStripper : CSharpSyntaxRewriter
 {
-    CommentStripper()
-    {
-    }
-
     public static SyntaxTree Strip(SyntaxTree syntaxTree)
     {
         var stripper = new CommentStripper();
@@ -38,76 +29,4 @@ class CommentStripper : CSharpSyntaxRewriter
     }
 }
 
-class EmptyDirectiveStripper : CSharpSyntaxRewriter
-{
-    public static SyntaxTree Strip(SyntaxTree syntaxTree)
-    {
-        var stripper = new EmptyDirectiveStripper();
-        var newRoot = stripper.Visit(syntaxTree.GetRoot());
-        return syntaxTree.WithRootAndOptions(newRoot, syntaxTree.Options);
-    }
-
-    public override bool VisitIntoStructuredTrivia => true;
-
-    public override SyntaxNode? VisitIfDirectiveTrivia(IfDirectiveTriviaSyntax node)
-    {
-        var ifLine = node.GetLocation().GetMappedLineSpan().StartLinePosition;
-        var nextToken = node.GetNextDirective();
-
-        if (nextToken != null)
-        {
-            var nextTokenParent = nextToken.Parent;
-            var nextLine = nextToken.GetLocation().GetMappedLineSpan().StartLinePosition;
-            if (nextToken.IsKind(SyntaxKind.EndIfDirectiveTrivia))
-            {
-                if (ifLine.Line + 1 == nextLine.Line)
-                {
-                    return null;
-                }
-            }
-        }
-
-        // if (nextToken.IsKind(SyntaxKind.ElseDirectiveTrivia))
-        // {
-        //     return null;
-        // }
-        //
-        // if (nextToken.IsKind(SyntaxKind.ElifDirectiveTrivia))
-        // {
-        //
-        //     return null;
-        // }
-
-        return base.VisitIfDirectiveTrivia(node);
-    }
-
-
-    public override SyntaxNode? VisitEndIfDirectiveTrivia(EndIfDirectiveTriviaSyntax node)
-    {
-        var previousDirective = node.GetPreviousDirective();
-
-        if (previousDirective != null &&
-            previousDirective.IsKind(SyntaxKind.IfDirectiveTrivia))
-        {
-            var endifLine = node.GetLocation().GetMappedLineSpan().StartLinePosition.Line;
-            var ifLine = previousDirective.GetLocation().GetMappedLineSpan().StartLinePosition.Line;
-
-            if (ifLine + 1 == endifLine)
-            {
-                return null; // Remove the empty #endif directive
-            }
-        }
-
-        return base.VisitEndIfDirectiveTrivia(node);
-    }
-    //
-    // public override SyntaxNode? VisitElifDirectiveTrivia(ElifDirectiveTriviaSyntax node) =>
-    //     null;
-    //
-    // public override SyntaxNode? VisitElseDirectiveTrivia(ElseDirectiveTriviaSyntax node) =>
-    //     null;
-    //
-    // public override SyntaxNode? VisitEndIfDirectiveTrivia(EndIfDirectiveTriviaSyntax node) =>
-    //     null;
-}
 #endif
