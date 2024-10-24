007fc04d3ec92cbc7a640175225a61167721bdcb
Simon Cropp <simon.cropp@gmail.com>
2024-10-24 17:25:29 +1100

    Update Identifier.cs

--- src/ApiBuilderTests/Identifier.cs
+++ src/ApiBuilderTests/Identifier.cs
@@ -1,7 +1,5 @@
-﻿#if NET9_0 && DEBUG
-public class Identifier
+﻿public class Identifier
 {
     public required string Moniker { get; init; }
     public required List<string> Directives { get; init; }
 }
-#endif
