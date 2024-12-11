9cea404cf13f247b411780ecd26a9415d12e9e85
Tom Longhurst <30480171+thomhurst@users.noreply.github.com>
2024-12-11 19:03:19 +0000

    Test

--- src/Tests/PolyfillTests_Guid.cs
+++ src/Tests/PolyfillTests_Guid.cs
@@ -1,9 +1,13 @@
+using System.Text.RegularExpressions;
+
 partial class PolyfillTests
 {
+    private static readonly Regex GuidV7Regex = new("^[0-9a-f]{8}(?:\\-[0-9a-f]{4}){3}-[0-9a-f]{12}$");
+
     [Test]
     public void GuidCreate7()
     {
-        var dayOfWeeks = GuidPolyfill.CreateVersion7();
-        Assert.AreEqual(DayOfWeek.Sunday, dayOfWeeks[0]);
+        var guid = GuidPolyfill.CreateVersion7();
+        Assert.IsTrue(GuidV7Regex.IsMatch(guid.ToString()));
     }
 }
