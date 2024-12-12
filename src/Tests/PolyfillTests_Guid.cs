687ee87df8f44958358ea2777bd2de2bd20ae4b9
Simon Cropp <simon.cropp@gmail.com>
2024-12-12 22:28:16 +1100

    Update PolyfillTests_Guid.cs

--- src/Tests/PolyfillTests_Guid.cs
+++ src/Tests/PolyfillTests_Guid.cs
@@ -2,12 +2,12 @@ using System.Text.RegularExpressions;
 
 partial class PolyfillTests
 {
-    private static readonly Regex GuidV7Regex = new("^[0-9a-f]{8}(?:\\-[0-9a-f]{4}){3}-[0-9a-f]{12}$");
+    static Regex guidV7Regex = new("^[0-9a-f]{8}(?:\\-[0-9a-f]{4}){3}-[0-9a-f]{12}$");
 
     [Test]
     public void GuidCreate7()
     {
         var guid = GuidPolyfill.CreateVersion7();
-        Assert.IsTrue(GuidV7Regex.IsMatch(guid.ToString()));
+        Assert.IsTrue(guidV7Regex.IsMatch(guid.ToString()));
     }
 }
