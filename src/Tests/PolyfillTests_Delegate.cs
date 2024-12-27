a8f74456b908f5ca71fd0c0e6aa153b9a4e888c7
Simon Cropp <simon.cropp@gmail.com>
2024-12-27 20:26:31 +1100

    .

--- src/Tests/PolyfillTests_Delegate.cs
+++ src/Tests/PolyfillTests_Delegate.cs
@@ -20,4 +20,19 @@ partial class PolyfillTests
     static void Handler(object? sender, EventArgs e)
     {
     }
+
+    [Test]
+    public void EnumerateInvocationList()
+    {
+        var count = 0;
+
+        MyEvent += (_, _) => count++;
+
+        foreach (var item in DelegatePolyfill.EnumerateInvocationList(MyEvent))
+        {
+            item(this, EventArgs.Empty);
+        }
+
+        Assert.AreEqual(1, count);
+    }
 }
