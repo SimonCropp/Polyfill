d2db41ddf4ba5f5e6f9160e9818f03e0c819db85
Simon Cropp <simon.cropp@gmail.com>
2025-01-05 21:44:34 +1100

    add string join overloads for ReadOnlySpan (#276)

--- src/Polyfill/StringPolyfill.cs
+++ src/Polyfill/StringPolyfill.cs
@@ -3,6 +3,7 @@
 
 namespace Polyfills;
 
+using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Diagnostics.CodeAnalysis;
@@ -18,7 +19,7 @@ static partial class StringPolyfill
     /// Concatenates an array of strings, using the specified separator between each member.
     /// </summary>
     //Link: https://learn.microsoft.com/en-us/dotnet/api/system.string.join#system-string-join(system-char-system-string())
-    public static string Join(char separator, string[] values) =>
+    public static string Join(char separator, params string[] values) =>
 #if NETSTANDARD2_0 || NETFRAMEWORK
         string.Join(new([separator]), values);
 #else
@@ -29,11 +30,57 @@ static partial class StringPolyfill
     /// Concatenates the string representations of an array of objects, using the specified separator between each member.
     /// </summary>
     //Link: https://learn.microsoft.com/en-us/dotnet/api/system.string.join#system-string-join(system-char-system-object())
-    public static string Join(char separator, object[] values) =>
+    public static string Join(char separator, params object[] values) =>
 #if NETSTANDARD2_0 || NETFRAMEWORK
         string.Join(new([separator]), values);
 #else
         string.Join(separator, values);
+#endif
+
+#if FeatureMemory
+    /// <summary>
+    /// Concatenates the string representations of a span of objects, using the specified separator between each member.
+    /// </summary>
+    //Link: https://learn.microsoft.com/en-us/dotnet/api/system.string.join#system-string-join(system-char-system-readonlyspan((system-object)))
+    public static string Join(char separator, scoped ReadOnlySpan<object?> values) =>
+#if NET9_0_OR_GREATER
+        string.Join(separator, values);
+#else
+        Join(separator, values.ToArray());
+#endif
+
+    /// <summary>
+    /// Concatenates a span of strings, using the specified separator between each member.
+    /// </summary>
+    //Link: https://learn.microsoft.com/en-us/dotnet/api/system.string.join#system-string-join(system-char-system-readonlyspan((system-string)))
+    public static string Join(char separator, scoped ReadOnlySpan<string?> values) =>
+#if NET9_0_OR_GREATER
+        string.Join(separator, values);
+#else
+        Join(separator, values.ToArray());
+#endif
+
+    /// <summary>
+    /// Concatenates the string representations of a span of objects, using the specified separator between each member.
+    /// </summary>
+    //Link: https://learn.microsoft.com/en-us/dotnet/api/system.string.join#system-string-join(system-string-system-readonlyspan((system-object)))
+    public static string Join(string separator, scoped ReadOnlySpan<object?> values) =>
+#if NET9_0_OR_GREATER
+        string.Join(separator, values);
+#else
+        string.Join(separator, values.ToArray());
+#endif
+
+    /// <summary>
+    /// Concatenates a span of strings, using the specified separator between each member.
+    /// </summary>
+    //Link: https://learn.microsoft.com/en-us/dotnet/api/system.string.join#system-string-join(system-string-system-readonlyspan((system-string)))
+    public static string Join(string separator, scoped ReadOnlySpan<string?> values) =>
+#if NET9_0_OR_GREATER
+        string.Join(separator, values);
+#else
+        string.Join(separator, values.ToArray());
+#endif
 #endif
 
     /// <summary>
