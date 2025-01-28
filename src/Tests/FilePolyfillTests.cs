b7f8da9f4af7fee44029eda7b30ed54e426b8cae
Simon Cropp <simon.cropp@gmail.com>
2025-01-28 22:34:56 +1100

    .

---
+++ src/Tests/FilePolyfillTests.cs
@@ -0,0 +1,74 @@
+﻿[TestFixture]
+public class FilePolyfillTests
+{
+    const string TestFilePath = "testfile.txt";
+
+    [SetUp]
+    public void SetUp()
+    {
+        if (File.Exists(TestFilePath))
+        {
+            File.Delete(TestFilePath);
+        }
+    }
+
+    [TearDown]
+    public void TearDown()
+    {
+        if (File.Exists(TestFilePath))
+        {
+            File.Delete(TestFilePath);
+        }
+    }
+
+    [Test]
+    public async Task AppendAllBytes()
+    {
+        var data = "Hello, World!"u8.ToArray();
+        // ReSharper disable once MethodHasAsyncOverload
+        FilePolyfill.AppendAllBytes(TestFilePath, data);
+
+        var result = await FilePolyfill.ReadAllBytesAsync(TestFilePath);
+        Assert.AreEqual(data, result);
+    }
+
+    [Test]
+    public async Task AppendAllBytesAsync()
+    {
+        var data = "Hello, Async World!"u8.ToArray();
+        await FilePolyfill.AppendAllBytesAsync(TestFilePath, data);
+
+        var result = await FilePolyfill.ReadAllBytesAsync(TestFilePath);
+        Assert.AreEqual(data, result);
+    }
+
+    [Test]
+    public async Task AppendAllTextAsync()
+    {
+        var content = "Hello, Async Text!";
+        await FilePolyfill.AppendAllTextAsync(TestFilePath, content, Encoding.UTF8);
+
+        var result = await FilePolyfill.ReadAllTextAsync(TestFilePath);
+        Assert.AreEqual(content, result);
+    }
+
+    [Test]
+    public async Task WriteAllBytesAsync()
+    {
+        var data = "Hello, Write Bytes!"u8.ToArray();
+        await FilePolyfill.WriteAllBytesAsync(TestFilePath, data);
+
+        var result = await FilePolyfill.ReadAllBytesAsync(TestFilePath);
+        Assert.AreEqual(data, result);
+    }
+
+    [Test]
+    public async Task WriteAllTextAsync()
+    {
+        var content = "Hello, Write Text!";
+        await FilePolyfill.WriteAllTextAsync(TestFilePath, content, Encoding.UTF8);
+
+        var result = await FilePolyfill.ReadAllTextAsync(TestFilePath);
+        Assert.AreEqual(content, result);
+    }
+}
