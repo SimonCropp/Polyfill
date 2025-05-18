fdfa355b190885af41bd76f330b99b781771406f
Simon Cropp <simon.cropp@gmail.com>
2025-05-19 07:40:11 +1000

    Create Extensions.cs

---
+++ src/Consume/Extensions.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public static class Enumerable
+{
+    extension<TSource>(IEnumerable<TSource> source)
+    {
+        public IEnumerable<TSource> Where(Func<TSource, bool> predicate)
+        {
+            return [];
+        }
+    }
+}
