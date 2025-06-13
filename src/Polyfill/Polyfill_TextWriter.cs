a66f7b7ab23ea53f9e2729a4b45ccaa43c6dbc9a
Simon Cropp <simon.cropp@gmail.com>
2025-06-13 12:53:40 +1000

    fix stack overflow in TextWriter.FlushAsync

--- src/Polyfill/Polyfill_TextWriter.cs
+++ src/Polyfill/Polyfill_TextWriter.cs
@@ -31,7 +31,7 @@ static partial class Polyfill
             return Task.FromCanceled(cancellationToken);
         }
 
-        return target.FlushAsync(cancellationToken)
+        return target.FlushAsync()
             .WaitAsync(cancellationToken);
     }
 
