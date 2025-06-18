91651f30a516db1554b793e5480f946dfe97b966
Simon Cropp <simon.cropp@gmail.com>
2025-06-18 18:53:58 +1000

    .

---
+++ src/ApiBuilderTests/LinkReader.cs
@@ -0,0 +1,30 @@
+﻿public static class LinkReader
+{
+    public static bool TryGetReference(this MethodDeclarationSyntax method, [NotNullWhen(true)] out string? reference)
+    {
+        var syntaxTrivia = method.GetLeadingTrivia();
+        foreach (var trivia in syntaxTrivia)
+        {
+            if (!trivia.IsKind(SyntaxKind.SingleLineCommentTrivia))
+            {
+                continue;
+            }
+
+            var comment = trivia.ToString();
+            if (!comment.StartsWith("//Link: "))
+            {
+                continue;
+            }
+
+            reference = comment.Replace("//Link: ", string.Empty);
+            if (reference.Contains("learn.") && !reference.Contains("?view=net-10.0"))
+            {
+                throw new($"Missing view: {reference}");
+            }
+            return true;
+        }
+
+        reference = null;
+        return false;
+    }
+}
