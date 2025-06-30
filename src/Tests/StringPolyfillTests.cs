011d41425bc65c883c29f47fe6b9188191cc8408
Simon Cropp <simon.cropp@gmail.com>
2025-06-30 20:26:52 +1000

    Update StringPolyfillTests.cs

--- src/Tests/StringPolyfillTests.cs
+++ src/Tests/StringPolyfillTests.cs
@@ -11,5 +11,7 @@ partial class StringPolyfillTest
         Assert.AreEqual("ba1a1c", StringPolyfill.Join("a1", ["b", null, "c"]));
         Assert.AreEqual("bac", StringPolyfill.Join('a', new object[] {"b", "c"}));
         Assert.AreEqual("baac", StringPolyfill.Join('a', new object?[] {"b", null, "c"}));
+        // ReSharper disable once RedundantCast
+        Assert.AreEqual("bac", StringPolyfill.Join('a', (IEnumerable<string>) new List<string>{"b", "c"}));
     }
 }
