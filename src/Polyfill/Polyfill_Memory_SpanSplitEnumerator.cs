1e23d95007d1bc05939974db3bfc18ee7ef5f52b
Simon Cropp <simon.cropp@gmail.com>
2025-07-07 11:29:22 +1000

    Update Polyfill_Memory_SpanSplitEnumerator.cs

--- src/Polyfill/Polyfill_Memory_SpanSplitEnumerator.cs
+++ src/Polyfill/Polyfill_Memory_SpanSplitEnumerator.cs
@@ -212,7 +212,6 @@ static partial class Polyfill
         {
         }
 
-
         const string whitespaces = "\t\n\v\f\r\u0020\u0085\u00a0\u1680\u2000\u2001\u2002\u2003\u2004\u2005\u2006\u2007\u2008\u2009\u200a\u2028\u2029\u202f\u205f\u3000";
 
 #if NET8_0
@@ -222,7 +221,6 @@ static partial class Polyfill
         public static readonly T[] WhiteSpaceChars;
 
         static SpanSplitEnumerator()
-
         {
             if (typeof(T) == typeof(char))
             {
