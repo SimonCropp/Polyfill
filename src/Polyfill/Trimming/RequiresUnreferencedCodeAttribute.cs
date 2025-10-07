c685380f65387e0b025eb360761072fc76833095
Simon Cropp <simon.cropp@gmail.com>
2025-10-07 20:35:21 +1100

    add ExcludeStatics to RequiresDynamicCode and RequiresUnreferencedCode (#399)

--- src/Polyfill/Trimming/RequiresUnreferencedCodeAttribute.cs
+++ src/Polyfill/Trimming/RequiresUnreferencedCodeAttribute.cs
@@ -34,6 +34,11 @@ sealed class RequiresUnreferencedCodeAttribute :
         Message = message;
 
     /// <summary>
+    /// When set to true, indicates that the annotation should not apply to static members.
+    /// </summary>
+    public bool ExcludeStatics { get; set; }
+
+    /// <summary>
     /// Gets a message that contains information about the usage of unreferenced code.
     /// </summary>
     public string Message { get; }
