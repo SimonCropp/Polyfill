fc1f2bc7fd2f20d8bc0df831afc767aa3581825d
PhilippNaused <philipp.naused@t-online.de>
2025-11-05 19:45:38 +0100

    Extend CI pipeline to test trimmed publishing

---
+++ src/ConsumeTrimmed/Program.cs
@@ -0,0 +1,21 @@
+using System.Diagnostics.CodeAnalysis;
+using System.Reflection;
+
+class Program
+{
+    private static void Main()
+    {
+        Test<Program>();
+    }
+
+    public void MethodA<T>() { }
+
+    private static void Test<[DynamicallyAccessedMembers(DynamicallyAccessedMemberTypes.PublicMethods | DynamicallyAccessedMemberTypes.NonPublicMethods)]T>()
+    {
+        var mInfo = typeof(T).GetMethod("MethodA", 1, BindingFlags.Public | BindingFlags.Instance, new Type[] { typeof(int) });
+        if (mInfo is null)
+        {
+            throw new InvalidOperationException("MethodA not found");
+        }
+    }
+}
