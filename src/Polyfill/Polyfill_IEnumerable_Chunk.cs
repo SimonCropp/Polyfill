6a78dd6656091081665f2b7cba6339a2a3393248
Simon Cropp <simon.cropp@gmail.com>
2025-11-13 21:43:22 +1100

    simplify Chunk

--- src/Polyfill/Polyfill_IEnumerable_Chunk.cs
+++ src/Polyfill/Polyfill_IEnumerable_Chunk.cs
@@ -21,54 +21,51 @@ static partial class Polyfill
             throw new ArgumentOutOfRangeException(nameof(size), size, "Size must be greater than 0.");
         }
 
-        return ChunkIterator(source, size);
+        using var enumerator = source.GetEnumerator();
 
-        static IEnumerable<TSource[]> ChunkIterator(IEnumerable<TSource> source, int size)
+        if (!enumerator.MoveNext())
         {
-            using var enumerator = source.GetEnumerator();
-
-            if (enumerator.MoveNext())
-            {
-                var arraySize = Math.Min(size, 4);
-                int i;
-                do
-                {
-                    var array = new TSource[arraySize];
-
-                    array[0] = enumerator.Current;
-                    i = 1;
-
-                    if (size != array.Length)
-                    {
-                        for (; i < size && enumerator.MoveNext(); i++)
-                        {
-                            if (i >= array.Length)
-                            {
-                                arraySize = (int) Math.Min((uint) size, 2 * (uint) array.Length);
-                                Array.Resize(ref array, arraySize);
-                            }
-
-                            array[i] = enumerator.Current;
-                        }
-                    }
-                    else
-                    {
-                        var local = array;
-                        for (; (uint) i < (uint) local.Length && enumerator.MoveNext(); i++)
-                        {
-                            local[i] = enumerator.Current;
-                        }
-                    }
-
-                    if (i != array.Length)
-                    {
-                        Array.Resize(ref array, i);
-                    }
-
-                    yield return array;
-                } while (i >= size && enumerator.MoveNext());
-            }
+            yield break;
         }
+
+        var arraySize = Math.Min(size, 4);
+        int i;
+        do
+        {
+            var array = new TSource[arraySize];
+
+            array[0] = enumerator.Current;
+            i = 1;
+
+            if (size != array.Length)
+            {
+                for (; i < size && enumerator.MoveNext(); i++)
+                {
+                    if (i >= array.Length)
+                    {
+                        arraySize = (int) Math.Min((uint) size, 2 * (uint) array.Length);
+                        Array.Resize(ref array, arraySize);
+                    }
+
+                    array[i] = enumerator.Current;
+                }
+            }
+            else
+            {
+                var local = array;
+                for (; (uint) i < (uint) local.Length && enumerator.MoveNext(); i++)
+                {
+                    local[i] = enumerator.Current;
+                }
+            }
+
+            if (i != array.Length)
+            {
+                Array.Resize(ref array, i);
+            }
+
+            yield return array;
+        } while (i >= size && enumerator.MoveNext());
     }
 }
 
