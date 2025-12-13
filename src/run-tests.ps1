3c644cc930dfedece5ff2b790c5a5391a1f2c3d4
Simon Cropp <simon.cropp@gmail.com>
2025-12-14 08:42:45 +1100

    Update run-tests.ps1

--- src/run-tests.ps1
+++ src/run-tests.ps1
@@ -1,10 +1,25 @@
-Get-ChildItem -Recurse -Filter "*Tests.csproj" -Path src | ForEach-Object {
+Get-ChildItem -Recurse -Filter "*Tests.csproj" -Path . | ForEach-Object {
     $proj = $_.FullName
+    $projName = $_.Name
     [xml]$xml = Get-Content $proj
-    $frameworks = ($xml.Project.PropertyGroup.TargetFrameworks ?? $xml.Project.PropertyGroup.TargetFramework) -split ';'
-    foreach ($fw in $frameworks | Where-Object { $_ }) {
-        Write-Host "Running $($_.Name) [$fw]" -ForegroundColor Cyan
+
+    # Collect all framework values from TargetFrameworks and TargetFramework elements
+    $allFrameworks = @()
+    foreach ($tf in $xml.Project.PropertyGroup.TargetFrameworks) {
+        if ($tf -is [string]) { $allFrameworks += $tf }
+        elseif ($tf) { $allFrameworks += $tf.InnerText }
+    }
+    foreach ($tf in $xml.Project.PropertyGroup.TargetFramework) {
+        if ($tf -is [string]) { $allFrameworks += $tf }
+        elseif ($tf) { $allFrameworks += $tf.InnerText }
+    }
+
+    # Split by semicolon, filter out empty and MSBuild variables
+    $frameworks = ($allFrameworks -join ';') -split ';' | Where-Object { $_ -and $_ -notmatch '^\$\(' }
+
+    foreach ($fw in $frameworks) {
+        Write-Host "Running $projName [$fw]" -ForegroundColor Cyan
         dotnet run --project $proj --configuration Release --framework $fw --no-build
-        if ($LASTEXITCODE -ne 0) { throw "Tests failed for $($_.Name) [$fw]" }
+        if ($LASTEXITCODE -ne 0) { throw "Tests failed for $projName [$fw]" }
     }
 }
