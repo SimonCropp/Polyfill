684631c6ff3ec277f8efbc7445487733f6802b6c
Alastair Lundy <133523182+alastairlundy@users.noreply.github.com>
2026-01-06 12:27:17 +0000

    add nint argument throw overloads

--- src/Polyfill/ArgumentExceptions/ArgumentOutOfRangeExceptionPolyfill.cs
+++ src/Polyfill/ArgumentExceptions/ArgumentOutOfRangeExceptionPolyfill.cs
@@ -150,12 +150,34 @@ static partial class Polyfill
 
 #if !NET8_0_OR_GREATER
         //Link: https://learn.microsoft.com/en-us/dotnet/api/system.argumentoutofrangeexception.throwifgreaterthanorequal?view=net-10.0#system-argumentoutofrangeexception-throwifgreaterthanorequal-1(-0-0-system-string)
+        public static void ThrowIfGreaterThan(nint value, nint other, [CallerArgumentExpression(nameof(value))] string? paramName = null)
+        {
+            if (value > other)
+            {
+                throw new ArgumentOutOfRangeException(paramName, value, $"Value must be less than or equal to {other}.");
+            }
+        }
+#endif
+
+#if !NET8_0_OR_GREATER
+        //Link: https://learn.microsoft.com/en-us/dotnet/api/system.argumentoutofrangeexception.throwifgreaterthanorequal?view=net-10.0#system-argumentoutofrangeexception-throwifgreaterthanorequal-1(-0-0-system-string)
         public static void ThrowIfGreaterThanOrEqual<T>(T value, T other, [CallerArgumentExpression(nameof(value))] string? paramName = null)
             where T : IComparable<T>
         {
             if (value.CompareTo(other) >= 0)
             {
                 throw new ArgumentOutOfRangeException(paramName, value, $"Value must be less than {other}.");
+            }
+        }
+#endif
+
+#if !NET8_0_OR_GREATER
+        //Link: https://learn.microsoft.com/en-us/dotnet/api/system.argumentoutofrangeexception.throwifgreaterthanorequal?view=net-10.0#system-argumentoutofrangeexception-throwifgreaterthanorequal-1(-0-0-system-string)
+        public static void ThrowIfGreaterThanOrEqual(nint value, nint other, [CallerArgumentExpression(nameof(value))] string? paramName = null)
+        {
+            if (value >= other)
+            {
+                throw new ArgumentOutOfRangeException(paramName, value, $"Value must be less than or equal to {other}.");
             }
         }
 #endif
@@ -173,6 +195,17 @@ static partial class Polyfill
 #endif
 
 #if !NET8_0_OR_GREATER
+        //Link: https://learn.microsoft.com/en-us/dotnet/api/system.argumentoutofrangeexception.throwiflessthan?view=net-10.0#system-argumentoutofrangeexception-throwiflessthan-1(-0-0-system-string)
+        public static void ThrowIfLessThan(nint value, nint other, [CallerArgumentExpression(nameof(value))] string? paramName = null)
+        {
+            if (value < other)
+            {
+                throw new ArgumentOutOfRangeException(paramName, value, $"Value must be greater than or equal to {other}.");
+            }
+        }
+#endif
+
+#if !NET8_0_OR_GREATER
         //Link: https://learn.microsoft.com/en-us/dotnet/api/system.argumentoutofrangeexception.throwiflessthanorequal?view=net-10.0#system-argumentoutofrangeexception-throwiflessthanorequal-1(-0-0-system-string)
         public static void ThrowIfLessThanOrEqual<T>(T value, T other, [CallerArgumentExpression(nameof(value))] string? paramName = null)
             where T : IComparable<T>
@@ -180,6 +213,17 @@ static partial class Polyfill
             if (value.CompareTo(other) <= 0)
             {
                 throw new ArgumentOutOfRangeException(paramName, value, $"Value must be greater than {other}.");
+            }
+        }
+#endif
+
+#if !NET8_0_OR_GREATER
+        //Link: https://learn.microsoft.com/en-us/dotnet/api/system.argumentoutofrangeexception.throwiflessthanorequal?view=net-10.0#system-argumentoutofrangeexception-throwiflessthanorequal-1(-0-0-system-string)
+        public static void ThrowIfLessThanOrEqual(nint value, nint other, [CallerArgumentExpression(nameof(value))] string? paramName = null)
+        {
+            if (value <= other)
+            {
+                throw new ArgumentOutOfRangeException(paramName, value, $"Value must be greater than or equal to {other}.");
             }
         }
 #endif
