c3c4f45f265b1e6c903c9d38ff1e011958495d49
Simon Cropp <simon.cropp@gmail.com>
2026-04-10 17:18:41 +1000

    Add Regex Count (#534)

--- src/Consume/Consume.cs
+++ src/Consume/Consume.cs
@@ -1151,6 +1151,15 @@ class Consume
 
 #endif
 
+    void Regex_Count_Methods()
+    {
+        var regex = new Regex("a");
+        var count = regex.Count("aaa");
+        count = Regex.Count("aaa", "a");
+        count = Regex.Count("aaa", "a", RegexOptions.None);
+        count = Regex.Count("aaa", "a", RegexOptions.None, TimeSpan.FromSeconds(1));
+    }
+
 #if FeatureMemory
 
     void Regex_Methods()
